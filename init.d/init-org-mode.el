8fd47ecb5320da2bbb95b32580d0ec6bbc6904d3
Yen-Chin,Lee <coldnew.tw@gmail.com>
2010-10-06 20:58:25 +0800

    update .gitignore

---
+++ init.d/init-org-mode.el
@@ -0,0 +1,24 @@
+;; init org mode
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+;; (when (require 'org nil 'noerror)
+(setq org-hide-leading-stars t)
+(setq org-log-done 'time)
+(setq org-tag-alist '(
+		      ("Programming" . ?p) 
+		      ("Lab"         . ?l) 
+		      ("Home"        . ?h)
+		      ))    
+
+(when (require 'init-vim nil 'noerror)
+  (vim:imap (kbd "M-t") 'org-insert-todo-heading)
+  (vim:imap (kbd "C-t") 'org-insert-todo-heading-respect-content))
+
+
+
+
+
+(provide 'init-org-mode)
+
+
+;; )
