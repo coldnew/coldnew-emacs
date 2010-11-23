36cb2cc7b53a29dcadb8445b7cfb7dc522f7866c
Yen-Chin,Lee <coldnew.tw@gmail.com>
2010-11-23 19:39:08 +0800

    update

--- rc.d/rc-ibuffer.el
+++ rc.d/rc-ibuffer.el
@@ -8,7 +8,7 @@
 
   (setq ibuffer-saved-filter-groups
 	'(("default"
-	   ("*Buffer*" (name . "\*"))
+	   ("*Buffer*" (name . "\\*.*\\*"))
 	   ("Help" (or (mode . woman-mode)
 		       (mode . man-mode)
 		       (mode . info-mode)
@@ -32,15 +32,27 @@
     (vim:nmap (kbd "C-x C-b") 'ibuffer))
 
 
-
   ;; Reverse group list
   (defadvice ibuffer-generate-filter-groups (after reverse-ibuffer-groups () activate)
     (setq ad-return-value (nreverse ad-return-value)))
+
+  ;; Switching to ibuffer puts the cursor on the most recent buffer
+  (defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+    "Open ibuffer with cursor pointed to most recent buffer name"
+    (let ((recent-buffer-name (buffer-name)))
+      ad-do-it
+      (ibuffer-jump-to-buffer recent-buffer-name)))
+  (ad-activate 'ibuffer)
+
+  ;; Kill ibuffer after quit
+  (defadvice ibuffer-quit (after kill-ibuffer activate)
+    "Kill the ibuffer buffer on exit."
+    (kill-buffer "*Ibuffer*"))
+  (ad-activate 'ibuffer-quit)
 
   (add-hook 'ibuffer-mode-hook
 	    (lambda ()
 	      (hl-line-mode)		; Enable hight-line
 	      (ibuffer-switch-to-saved-filter-groups "default")))
-
 
   )
