7b9b720e533ce4e3eeefebc1dd8007c5b5393def
Yen-Chin,Lee <coldnew.tw@gmail.com>
2010-11-28 20:02:40 +0800

    update

--- rc.d/rc-color-theme.el
+++ rc.d/rc-color-theme.el
@@ -12,3 +12,12 @@
 ;;;; Use coldnew's personal theme default
 (when (require 'color-theme-coldnew-night nil 'noerror)
   (eval-after-load "color-theme" '(color-theme-coldnew-night)))
+
+;;;; Specified special color-theme according different modes
+;;(add-hook 'after-change-major-mode-hook;;
+(add-hook 'post-command-hook
+	  '(lambda ()
+	     (if (derived-mode-p 'w3m-mode)
+		 (color-theme-xp)
+	       (color-theme-coldnew-night)
+	       )))
