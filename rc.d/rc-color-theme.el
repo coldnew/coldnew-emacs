a0547fe98f070dcfd3309c719b6e33a2461f1614
Yen-Chin,Lee <coldnew.tw@gmail.com>
2010-11-28 20:51:16 +0800

    update

--- rc.d/rc-color-theme.el
+++ rc.d/rc-color-theme.el
@@ -9,15 +9,34 @@
   (setq color-theme-is-cumulative t)
   (setq color-theme-load-all-themes nil))
 
-;;;; Use coldnew's personal theme default
-(when (require 'color-theme-coldnew-night nil 'noerror)
-  (eval-after-load "color-theme" '(color-theme-coldnew-night)))
+(require 'color-theme-coldnew-day nil 'noerror)
+(require 'color-theme-coldnew-night nil 'noerror)
+
+;; default theme use night.
+(color-theme-coldnew-night)
+
 
 ;;;; Specified special color-theme according different modes
 ;;(add-hook 'after-change-major-mode-hook;;
 (add-hook 'post-command-hook
 	  '(lambda ()
 	     (if (derived-mode-p 'w3m-mode)
-		 (color-theme-xp)
-	       (color-theme-coldnew-night)
+		 (color-theme-coldnew-day)
+	       (if (not (minibufferp))
+		   (case *color-mode*
+		     ('night (color-theme-coldnew-night))
+		     ('day   (color-theme-coldnew-day))))
 	       )))
+
+;;;;;; Functions
+(defvar *color-mode* 'night
+  "Color mode for ``switch-day-and-night''.Can be 'day or 'night.")
+(defun switch-day-and-night ()
+  "Switch between day and nigh color-themes."
+  (interactive)
+  (case *color-mode*
+    ('day (color-theme-coldnew-day)
+	  (setq *color-mode* 'night))
+    ('night (color-theme-coldnew-night)
+	    (setq *color-mode* 'day))
+    ))
