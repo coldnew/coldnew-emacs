5e257bcbcb2d1bb075af5bd8a413d5cb47375e3b
Yen-Chin,Lee <coldnew.tw@gmail.com>
2010-12-01 09:59:44 +0800

    update

--- rc.d/rc-ibuffer.el
+++ rc.d/rc-ibuffer.el
@@ -7,77 +7,78 @@
     (setq ibuffer-expert t))
 
   (setq ibuffer-saved-filter-groups
-    '(("default"
-       ("*Buffer*" (or ;(name . "^\\*scratch\\*$")
-            (name . "^\\*Messages\\*$")
-            (name . "^TAGS\\(<[0-9]+>\\)?$")
-            (name . "^\\*Occur\\*$")
-            (name . "^\\*grep\\*$")
-            (name . "^\\*Compile-Log\\*$")
-            (name . "^\\*Backtrace\\*$")
-            (name . "^\\*Process List\\*$")
-            (name . "^\\*gud\\*$")
-            (name . "^\\*Kill Ring\\*$")
-            (name . "^\\*Completions\\*$")
-            (name . "^\\*tramp")
-            (name . "^\\*shell\\*$")
-            (name . "^\\*compilation\\*$")))
-       ("Version Control" (or (mode . svn-status-mode)
-                  (mode . svn-log-edit-mode)
-                  (name . "^\\*svn-")
-                  (name . "^\\*vc\\*$")
-                  (name . "^\\*Annotate")
-                  (name . "^\\*git-")
-                  (name . "^\\*vc-")))
-       ("Help" (or (mode . woman-mode)
-               (mode . man-mode)
-               (mode . info-mode)
-               (mode . help-mode)))
-       ("IRC"   (or (mode . erc-mode)
-            (mode . rcirc-mode)))
-       ("Terminal" (or (mode . eshell-mode)
-               (mode . term-mode)))
-       ("w3m"   (or (mode . w3m-mode)
-            (name . "^\\*w3m*")))
-       ("Shell Script" (or (mode . shell-mode)
-                   (mode . python-mode)
-                   (mode . perl-mode)
-                   (mode . ruby-mode)))
-       ("C++ . C#" (or (mode . c++-mode)
-               (mode . csharpmode)))
-       ("C . Obj-C" (or (mode . c-mode)
-                (mode . objc-mode)))
-       ("Emacs" (mode . emacs-lisp-mode))
-       )))
+	'(("default"
+	   ("*Buffer*" (or (name . "^\\*Messages\\*$")
+			   (name . "^TAGS\\(<[0-9]+>\\)?$")
+			   (name . "^\\*Occur\\*$")
+			   (name . "^\\*grep\\*$")
+			   (name . "^\\*Compile-Log\\*$")
+			   (name . "^\\*Backtrace\\*$")
+			   (name . "^\\*Process List\\*$")
+			   (name . "^\\*gud\\*$")
+			   (name . "^\\*Kill Ring\\*$")
+			   (name . "^\\*Completions\\*$")
+			   (name . "^\\*tramp")
+			   (name . "^\\*shell\\*$")
+			   (name . "^\\*compilation\\*$")
+			   (name . "^\\*Buffer List\\*$"))
+	    ("Version Control" (or (mode . svn-status-mode)
+				   (mode . svn-log-edit-mode)
+				   (name . "^\\*svn-")
+				   (name . "^\\*vc\\*$")
+				   (name . "^\\*Annotate")
+				   (name . "^\\*git-")
+				   (name . "^\\*cvs")
+				   (name . "^\\*vc-")))
+	    ("Help" (or (mode . woman-mode)
+			(mode . man-mode)
+			(mode . info-mode)
+			(mode . help-mode)))
+	    ("IRC"   (or (mode . erc-mode)
+			 (mode . rcirc-mode)))
+	    ("Terminal" (or (mode . eshell-mode)
+			    (mode . term-mode)))
+	    ("w3m"   (or (mode . w3m-mode)
+			 (name . "^\\*w3m*")))
+	    ("Shell Script" (or (mode . shell-mode)
+				(mode . python-mode)
+				(mode . perl-mode)
+				(mode . ruby-mode)))
+	    ("C++ . C#" (or (mode . c++-mode)
+			    (mode . csharpmode)))
+	    ("C . Obj-C" (or (mode . c-mode)
+			     (mode . objc-mode)))
+	    ("Emacs" (mode . emacs-lisp-mode))
+	    )))
 
-  ;; Keybinding
-  (when (require 'vim nil 'noerror)
-    ;; BUG: (require 'vim-ibuffer nil 'noerror)
-    (vim:nmap (kbd "C-x C-b") 'ibuffer)
-    (vim:imap (kbd "C-x C-b") 'ibuffer))
+	;; Keybinding
+	(when (require 'vim nil 'noerror)
+	  ;; BUG: (require 'vim-ibuffer nil 'noerror)
+	  (vim:nmap (kbd "C-x C-b") 'ibuffer)
+	  (vim:imap (kbd "C-x C-b") 'ibuffer))
 
 
-  ;; Reverse group list
-  (defadvice ibuffer-generate-filter-groups (after reverse-ibuffer-groups () activate)
-    (setq ad-return-value (nreverse ad-return-value)))
+	;; Reverse group list
+	(defadvice ibuffer-generate-filter-groups (after reverse-ibuffer-groups () activate)
+	  (setq ad-return-value (nreverse ad-return-value)))
 
-  ;; Switching to ibuffer puts the cursor on the most recent buffer
-  (defadvice ibuffer (around ibuffer-point-to-most-recent) ()
-    "Open ibuffer with cursor pointed to most recent buffer name"
-    (let ((recent-buffer-name (buffer-name)))
-      ad-do-it
-      (ibuffer-jump-to-buffer recent-buffer-name)))
-  (ad-activate 'ibuffer)
+	;; Switching to ibuffer puts the cursor on the most recent buffer
+	(defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+	  "Open ibuffer with cursor pointed to most recent buffer name"
+	  (let ((recent-buffer-name (buffer-name)))
+	    ad-do-it
+	    (ibuffer-jump-to-buffer recent-buffer-name)))
+	(ad-activate 'ibuffer)
 
-  ;; Kill ibuffer after quit
-  (defadvice ibuffer-quit (after kill-ibuffer activate)
-    "Kill the ibuffer buffer on exit."
-    (kill-buffer "*Ibuffer*"))
-  (ad-activate 'ibuffer-quit)
+	;; Kill ibuffer after quit
+	(defadvice ibuffer-quit (after kill-ibuffer activate)
+	  "Kill the ibuffer buffer on exit."
+	  (kill-buffer "*Ibuffer*"))
+	(ad-activate 'ibuffer-quit)
 
-  (add-hook 'ibuffer-mode-hook
-        (lambda ()
-          (hl-line-mode)		; Enable hight-line
-          (ibuffer-switch-to-saved-filter-groups "default")))
+	(add-hook 'ibuffer-mode-hook
+		  (lambda ()
+		    (hl-line-mode)		; Enable hight-line
+		    (ibuffer-switch-to-saved-filter-groups "default")))
 
-  )
+	)
