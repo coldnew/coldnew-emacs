2e519ac0dfc0d3890c38af7504d6a3aa56f57ae3
Yen-Chin,Lee <coldnew.tw@gmail.com>
2010-12-01 21:47:59 +0800

    update

--- rc.d/rc-cpp-mode.el
+++ rc.d/rc-cpp-mode.el
@@ -12,30 +12,33 @@
 ;; Coding-Style Setting
 (add-hook 'c++-mode-hook
 	  '(lambda ()
+	     (c-set-style "linux")                ; C++ 語言風格為 linux
 	     (setq c-basic-offset             8 ) ; 縮排設定
 	     (setq tab-width                  4 ) ; TAB 寬度為4個空格
 	     (setq indent-tabs-mode         nil ) ; 禁止在縮排中插入制表符
 	     (setq c-max-one-liner-length   100 ) ; 最大格數為 100 格
 	     (setq compilation-window-height 10 ) ; 編譯結果視窗高度
-	     (c-set-style "linux")                ; C++ 語言風格為 linux
 	     (c-toggle-auto-state t)		  ; Auto indent on insertion of a curly brace
-	     ;;
-	     (add-to-list ff-other-file-alist '((("\\.cpp$"   (".h" ".hpp")))))
-	     ;; hook for cpp-mode
-	     (programming-common-hook)	; programming common hook
 	     ))
 
 ;;;; Keybinding
 (add-hook 'c++-mode-hook
 	  '(lambda ()
-	     (when (require 'vim nil 'noerror)
-	       (vim:nmap (kbd ",o") 'ff-find-other-file)
-	       (vim:nmap (kbd ",h") 'ff-find-related-file)
-	       (vim:imap (kbd "=")   'cpp-mode:insert-equal)
-	       (vim:imap (kbd "M-i") 'cpp-mode:insert-inc-or-if)
-	       (vim:imap (kbd "M-d") 'cpp-mode:insert-do-while)
-	       )))
+	     (vim:nmap (kbd ",o") 'ff-find-other-file)
+	     (vim:nmap (kbd ",h") 'ff-find-related-file)
+	     (vim:imap (kbd "=")   'cpp-mode:insert-equal)
+	     (vim:imap (kbd "M-i") 'cpp-mode:insert-inc-or-if)
+	     (vim:imap (kbd "M-d") 'cpp-mode:insert-do-while)
+	     ))
 
+;;;; Hooks
+(add-hook 'c++-mode-hook
+	  '(lambda ()
+	     ;;
+	     (add-to-list ff-other-file-alist '((("\\.cpp$"   (".h" ".hpp")))))
+	     ;; hook for cpp-mode
+	     (programming-common-hook)	; programming common hook
+	     ))
 
 ;;;; Functions
 
