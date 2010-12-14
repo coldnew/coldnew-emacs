4a09070aa93485838811acc680ca88559d56f7b9
Yen-Chin,Lee <coldnew.tw@gmail.com>
2010-12-14 10:58:41 +0800

    update

--- rc.d/rc-org-mode.el
+++ rc.d/rc-org-mode.el
@@ -32,6 +32,60 @@
 	       )))
 
 
+;;;; Extra Settings
+;; allow for export=>beamer by placing
+;; see http://emacs-fu.blogspot.com/2009/10/writing-presentations-with-org-mode-and.html
+;; #+LaTeX_CLASS: beamer in org files
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+(add-to-list 'org-export-latex-classes
+	     ;; beamer class, for presentations
+	     '("beamer"
+	       "\\documentclass[11pt]{beamer}\n
+		\\mode<{{{beamermode}}}>\n
+      		\\usetheme{{{{beamertheme}}}}\n
+  		\\usecolortheme{{{{beamercolortheme}}}}\n
+		\\beamertemplateballitem\n
+		\\setbeameroption{show notes}
+		\\usepackage[utf8]{inputenc}\n
+		\\usepackage[T1]{fontenc}\n
+		\\usepackage{hyperref}\n
+		\\usepackage{color}
+		\\usepackage{listings}
+		\\lstset{numbers=none,language=[ISO]C++,tabsize=4,
+			frame=single,
+			basicstyle=\\small,
+			showspaces=false,showstringspaces=false,
+			showtabs=false,
+			keywordstyle=\\color{blue}\\bfseries,
+			commentstyle=\\color{red},
+			}\n
+		 \\usepackage{verbatim}\n
+		 \\institute{{{{beamerinstitute}}}}\n
+		  \\subject{{{{beamersubject}}}}\n"
+
+	       ("\\section{%s}" . "\\section*{%s}")
+
+	       ("\\begin{frame}[fragile]\\frametitle{%s}"
+		"\\end{frame}"
+		"\\begin{frame}[fragile]\\frametitle{%s}"
+		"\\end{frame}")))
+
+;; letter class, for formal letters
+
+(add-to-list 'org-export-latex-classes
+
+	     '("letter"
+	       "\\documentclass[11pt]{letter}\n
+		\\usepackage[utf8]{inputenc}\n
+		\\usepackage[T1]{fontenc}\n
+		 \\usepackage{color}"
+
+	       ("\\section{%s}" . "\\section*{%s}")
+	       ("\\subsection{%s}" . "\\subsection*{%s}")
+	       ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+	       ("\\paragraph{%s}" . "\\paragraph*{%s}")
+	       ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 
 ;;;;;; Functions
