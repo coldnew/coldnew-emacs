27414dbe56a9b1f1d22ab0983a55361fc338f96a
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-01-28 10:17:11 +0800

    update

---
+++ rc.d/034-shell-pop.el
@@ -0,0 +1,10 @@
+
+
+(shell-pop-set-internal-mode "ansi-term")
+(shell-pop-set-internal-mode-shell "/bin/bash")
+(shell-pop-set-window-height 20)
+(shell-pop-set-window-position "bottom")
+
+
+(provide '034-shell-pop)
+;;  034-shell-pop.el ends here.
