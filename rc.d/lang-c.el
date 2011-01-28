4d1fc9e99ac4ec0c2ff4f6256dd91c212b8f7e16
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-01-28 13:55:04 +0800

    update

--- rc.d/lang-c.el
+++ rc.d/lang-c.el
@@ -4,45 +4,44 @@
 
 ;;;; Coding-Style Setting
 (add-hook 'c-mode-hook
-	  '(lambda ()
-	     (c-set-style "linux")                ; C 語言風格為 linux
-	     (setq c-basic-offset             8 ) ; 縮排設定
-	     (setq tab-width                  4 ) ; TAB 寬度為4個空格
-	     (setq indent-tabs-mode         nil ) ; 禁止在縮排中插入制表符
-	     (setq c-max-one-liner-length   100 ) ; 最大格數為 100 格
-	     (setq compilation-window-height 10 ) ; 編譯結果視窗高度
-	     ;;(substatement-open   .   0)
-	     ))
+      '(lambda ()
+         (c-set-style "linux")                ; C 語言風格為 linux
+         (setq c-basic-offset             8 ) ; 縮排設定
+         (setq tab-width                  4 ) ; TAB 寬度為4個空格
+         (setq indent-tabs-mode         nil ) ; 禁止在縮排中插入制表符
+         (setq c-max-one-liner-length   100 ) ; 最大格數為 100 格
+         (setq compilation-window-height 10 ) ; 編譯結果視窗高度
+         ;;(substatement-open   .   0)
+         ))
 
 ;;;; Other Settings
 (setq c-mode:include-dirs		; Setting include directories
       '(
-	"/usr/include"
-	"/usr/local/include"
-	))
+    "/usr/include"
+    "/usr/local/include"
+    ))
 
 ;;;; Keybindings
 (add-hook 'c-mode-hook
-	  '(lambda ()
-	     (vim:local-nmap (kbd ",o") 'ff-find-other-file)
-	     (vim:local-nmap (kbd ",h") 'ff-find-related-file)
-	     ;; Insert yasnippet
-	     (vim:local-imap (kbd "M-i") 'c-mode:insert-inc-or-if) ; insert "#include <>" or "if () {...}"
-	     (vim:local-imap (kbd "M-d") 'c-mode:insert-do-while)  ; insert "do {...} while()"
-	     (vim:local-imap (kbd "M-m") 'c-mode:insert-main-function) ; insert "int main () {...}"
-	     ))
+      '(lambda ()
+         (vim:local-nmap (kbd ",o") 'ff-find-other-file)
+         (vim:local-nmap (kbd ",h") 'ff-find-related-file)
+         ;; Insert yasnippet
+         (vim:local-imap (kbd "M-i") 'c-mode:insert-inc-or-if) ; insert "#include <>" or "if () {...}"
+         (vim:local-imap (kbd "M-d") 'c-mode:insert-do-while)  ; insert "do {...} while()"
+         (vim:local-imap (kbd "M-m") 'c-mode:insert-main-function) ; insert "int main () {...}"
+         ))
 
 ;;;; Hooks
 (add-hook 'c-mode-hook
-	  '(lambda ()
-	     ;; Use my define programming-common environment
-	     (programming-common-hook)
-	     ;; Use my define cc-mode common environment
-	     (cc-mode-common-hook)
-	     ;; Enable C-Eldoc
-	     (c-turn-on-eldoc-mode)
-	     ))
-
+      '(lambda ()
+         ;; Use my define programming-common environment
+         (programming-common-hook)
+         ;; Use my define cc-mode common environment
+         (cc-mode-common-hook)
+         ;; Enable C-Eldoc
+         (c-turn-on-eldoc-mode)
+         ))
 ;;;; Auto-Mode
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c-mode))
 (add-to-list 'auto-mode-alist '("\\.c\\'" . c-mode))
@@ -64,32 +63,34 @@
 
 ;;;; Functions
 
+
+
 ;; insert yasnippet
 (defcmd c-mode:insert-inc-or-if ()
   "If at the start of line. add `inc' and expand it,
 else add `if' and expand it."
   (let* ((current (point))
-	 (begin (line-beginning-position)))
+     (begin (line-beginning-position)))
     (if (eq current begin)
-	(progn
-	  (c-mode:skeleton-include)
-	  (newline-and-indent))
+    (progn
+      (c-mode:skeleton-include)
+      (newline-and-indent))
       (progn
-	(insert "if")
-	(yas/expand)))))
+    (insert "if")
+    (yas/expand)))))
 
 ;; FIXME: We don't want directories also show in completion
 (define-skeleton c-mode:skeleton-include
   "generate include<>" ""
   > "#include <"
   (completing-read "Enter include file："
-		   (mapcar #'(lambda (f) (list f ))
-			   (apply 'append
-				  (mapcar
-				   #'(lambda (dir)
-				       (directory-files dir))
-				   c-mode:include-dirs
-				   ))))
+           (mapcar #'(lambda (f) (list f ))
+               (apply 'append
+                  (mapcar
+                   #'(lambda (dir)
+                       (directory-files dir))
+                   c-mode:include-dirs
+                   ))))
   ">")
 
 
@@ -101,9 +102,9 @@ else add `if' and expand it."
 (defcmd c-mode:insert-main-function ()
   "insert main()."
   (let* ((current (point))
-	 (begin (line-beginning-position)))
+     (begin (line-beginning-position)))
     (if (equal current begin)
-	(insert "main"))
+    (insert "main"))
     (yas/expand)))
 
 (provide 'lang-c)
--- rc.d/rc-ccmode-common.el
+++ rc.d/rc-ccmode-common.el
@@ -10,26 +10,56 @@
   (vim:local-imap (kbd "<")   'cc-mode:insert-lesser-or-shift)
   )
 
-;(font-lock-add-keywords 'nil
- ;           '(("\\(\\w+\\)\\s-*\("
-  ;             (1 rumpsteak-font-lock-function-call-face))) t)
+                    ;(font-lock-add-keywords 'nil
+                    ;           '(("\\(\\w+\\)\\s-*\("
+                    ;             (1 rumpsteak-font-lock-function-call-face))) t)
 
 
-;(defun rumpsteak-match-function-call (&optional limit)
- ; (while (and (search-forward-regexp "\\(\\w+\\)\\s-*\(" limit 'no-error)
-  ;            (not (save-match-data
-   ;                  (string-match c-keywords-regexp (match-string 1))))
-    ;          (not (save-excursion
-     ;                (backward-char)
-      ;               (forward-sexp)
-       ;              (c-skip-whitespace-forward)
-        ;             (or (eobp) (= ?\{ (char-after (point)))))))))
-;(font-lock-add-keywords 'c-mode
- ;           '((rumpsteak-match-function-call
-  ;             (1 rumpsteak-font-lock-function-call-face))))
+                    ;(defun rumpsteak-match-function-call (&optional limit)
+                    ; (while (and (search-forward-regexp "\\(\\w+\\)\\s-*\(" limit 'no-error)
+                    ;            (not (save-match-data
+                    ;                  (string-match c-keywords-regexp (match-string 1))))
+                    ;          (not (save-excursion
+                    ;                (backward-char)
+                    ;               (forward-sexp)
+                    ;              (c-skip-whitespace-forward)
+                    ;             (or (eobp) (= ?\{ (char-after (point)))))))))
+                    ;(font-lock-add-keywords 'c-mode
+                    ;           '((rumpsteak-match-function-call
+                    ;             (1 rumpsteak-font-lock-function-call-face))))
 
 
 ;;;; Functions
+(defun high-light-if0 (limit)
+  (save-restriction
+    (widen)
+    (save-excursion
+      (goto-char (point-min))
+      (let ((depth 0) str start start-depth)
+        (while (re-search-forward "^\\s-*#\\s-*\\(if\\|else\\|endif\\)" limit 'move)
+          (setq str (match-string 1))
+          (if (string= str "if")
+              (progn
+                (setq depth (1+ depth))
+                (when (and (null start) (looking-at "\\s-+0"))
+                  (setq start (match-end 0)
+                        start-depth depth)))
+            (when (and start (= depth start-depth))
+              (c-put-font-lock-face start (match-beginning 0) 'font-lock-comment-face)
+              (setq start nil))
+            (when (string= str "endif")
+              (setq depth (1- depth)))))
+        (when (and start (> depth 0))
+          (c-put-font-lock-face start (point) 'font-lock-comment-face)))))
+  nil)
+
+(defun my-c-mode-common-hook ()
+  (font-lock-add-keywords
+   nil
+   '((high-light-if0 (0 font-lock-comment-face prepend))) 'add-to-end))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
 
 (defun high-light-functions ()
   "hightlight functions if it's regexp match func() "
