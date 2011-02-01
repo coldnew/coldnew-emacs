b8dc5dbd77b282dbfe948ceee4c8c63ce0992d90
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-02 01:05:01 +0800

    "asd"

---
+++ lisp/.loaddefs.el
@@ -0,0 +1,487 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (anything-other-buffer anything-at-point anything)
+;;;;;;  "anything/anything" "anything/anything.el" (19777 35650))
+;;; Generated autoloads from anything/anything.el
+
+(autoload 'anything "anything/anything" "\
+Select anything. In Lisp program, some optional arguments can be used.
+
+PLIST is a list like (:key1 val1 :key2 val2 ...) or
+\(&optional sources input prompt resume preselect buffer keymap).
+
+Basic keywords are the following:
+
+- :sources
+
+Temporary value of `anything-sources'.  It also accepts a
+symbol, interpreted as a variable of an anything source.  It
+also accepts an alist representing an anything source, which is
+detected by (assq 'name ANY-SOURCES)
+
+- :input
+
+Temporary value of `anything-pattern', ie. initial input of minibuffer.
+
+- :prompt
+
+Prompt other than \"pattern: \".
+
+- :resume
+
+If t, Resurrect previously instance of `anything'. Skip the initialization.
+If 'noresume, this instance of `anything' cannot be resumed.
+
+- :preselect
+
+Initially selected candidate. Specified by exact candidate or a regexp.
+Note that it is not working with delayed sources.
+
+- :buffer
+
+`anything-buffer' instead of *anything*.
+
+- :keymap
+
+`anything-map' for current `anything' session.
+
+
+Of course, conventional arguments are supported, the two are same.
+
+\(anything :sources sources :input input :prompt prompt :resume resume
+:preselect preselect :buffer buffer :keymap keymap)
+\(anything sources input prompt resume preselect buffer keymap)
+
+
+Other keywords are interpreted as local variables of this anything session.
+The `anything-' prefix can be omitted. For example,
+
+\(anything :sources 'anything-c-source-buffers
+:buffer \"*buffers*\" :candidate-number-limit 10)
+
+means starting anything session with `anything-c-source-buffers'
+source in *buffers* buffer and set
+`anything-candidate-number-limit' to 10 as session local variable.
+
+\(fn &rest PLIST)" t nil)
+
+(autoload 'anything-at-point "anything/anything" "\
+Same as `anything' except when C-u is pressed, the initial input is the symbol at point.
+
+\(fn &optional ANY-SOURCES ANY-INPUT ANY-PROMPT ANY-RESUME ANY-PRESELECT ANY-BUFFER)" t nil)
+
+(autoload 'anything-other-buffer "anything/anything" "\
+Simplified interface of `anything' with other `anything-buffer'
+
+\(fn ANY-SOURCES ANY-BUFFER)" nil nil)
+
+;;;***
+
+;;;### (autoloads (csharp-mode csharp-mode-hook) "csharp-mode/csharp-mode"
+;;;;;;  "csharp-mode/csharp-mode.el" (19782 12821))
+;;; Generated autoloads from csharp-mode/csharp-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
+
+(defvar csharp-mode-hook nil "\
+*Hook called by `csharp-mode'.")
+
+(custom-autoload 'csharp-mode-hook "csharp-mode/csharp-mode" t)
+
+(autoload 'csharp-mode "csharp-mode/csharp-mode" "\
+Major mode for editing C# code. This mode is derived from CC Mode to
+support C#.
+
+The hook `c-mode-common-hook' is run with no args at mode
+initialization, then `csharp-mode-hook'.
+
+This mode will automatically add a regexp for Csc.exe error and warning
+messages to the `compilation-error-regexp-alist'.
+
+Key bindings:
+\\{csharp-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "doxymacs/lisp/doxymacs" "doxymacs/lisp/doxymacs.el"
+;;;;;;  (19777 4986))
+;;; Generated autoloads from doxymacs/lisp/doxymacs.el
+
+(or (assoc 'doxymacs-mode minor-mode-alist) (setq minor-mode-alist (cons '(doxymacs-mode " doxy") minor-mode-alist)))
+
+;;;***
+
+;;;### (autoloads (xml-reformat-tags insert-xml read-xml) "doxymacs/lisp/xml-parse"
+;;;;;;  "doxymacs/lisp/xml-parse.el" (19777 4977))
+;;; Generated autoloads from doxymacs/lisp/xml-parse.el
+
+(autoload 'read-xml "doxymacs/lisp/xml-parse" "\
+Parse XML data at point into a Lisp structure.
+See `insert-xml' for a description of the format of this structure.
+Point is left at the end of the XML structure read.
+
+\(fn &optional PROGRESS-CALLBACK)" nil nil)
+
+(autoload 'insert-xml "doxymacs/lisp/xml-parse" "\
+Insert DATA, a recursive Lisp structure, at point as XML.
+DATA has the form:
+
+  ENTRY       ::=  (TAG CHILD*)
+  CHILD       ::=  STRING | ENTRY
+  TAG         ::=  TAG_NAME | (TAG_NAME ATTR+)
+  ATTR        ::=  (ATTR_NAME . ATTR_VALUE)
+  TAG_NAME    ::=  STRING
+  ATTR_NAME   ::=  STRING
+  ATTR_VALUE  ::=  STRING
+
+If ADD-NEWLINES is non-nil, newlines and indentation will be added to
+make the data user-friendly.
+
+If PUBLIC and SYSTEM are non-nil, a !DOCTYPE tag will be added at the
+top of the document to identify it as an XML document.
+
+DEPTH is normally for internal use only, and controls the depth of the
+indentation.
+
+\(fn DATA &optional ADD-NEWLINES PUBLIC SYSTEM DEPTH RET-DEPTH)" nil nil)
+
+(autoload 'xml-reformat-tags "doxymacs/lisp/xml-parse" "\
+If point is on the open bracket of an XML tag, reformat that tree.
+Note that this only works if the opening tag starts at column 0.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (gnugo) "gnugo/gnugo" "gnugo/gnugo.el" (19782 16613))
+;;; Generated autoloads from gnugo/gnugo.el
+
+(autoload 'gnugo "gnugo/gnugo" "\
+Run gnugo in a buffer, or resume a game in progress.
+Prefix arg means skip the game-in-progress check and start a new
+game straight away.
+
+You are queried for additional command-line options (Emacs supplies
+\"--mode gtp --quiet\" automatically).  Here is a list of options
+that gnugo.el understands and handles specially:
+
+--boardsize num   Set the board size to use (5--19)
+--color <color>   Choose your color ('black' or 'white')
+--handicap <num>  Set the number of handicap stones (0--9)
+
+If there is already a game in progress you may resume it instead of
+starting a new one.  See `gnugo-board-mode' documentation for more info.
+
+\(fn &optional NEW-GAME)" t nil)
+
+;;;***
+
+;;;### (autoloads (highlight-parentheses-mode) "highlight-parentheses/highlight-parentheses"
+;;;;;;  "highlight-parentheses/highlight-parentheses.el" (19777 341))
+;;; Generated autoloads from highlight-parentheses/highlight-parentheses.el
+
+(autoload 'highlight-parentheses-mode "highlight-parentheses/highlight-parentheses" "\
+Minor mode to highlight the surrounding parentheses.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (htmlize-many-files-dired htmlize-many-files htmlize-file
+;;;;;;  htmlize-region htmlize-buffer) "htmlize/htmlize" "htmlize/htmlize.el"
+;;;;;;  (19782 12821))
+;;; Generated autoloads from htmlize/htmlize.el
+
+(autoload 'htmlize-buffer "htmlize/htmlize" "\
+Convert BUFFER to HTML, preserving colors and decorations.
+
+The generated HTML is available in a new buffer, which is returned.
+When invoked interactively, the new buffer is selected in the current
+window.  The title of the generated document will be set to the buffer's
+file name or, if that's not available, to the buffer's name.
+
+Note that htmlize doesn't fontify your buffers, it only uses the
+decorations that are already present.  If you don't set up font-lock or
+something else to fontify your buffers, the resulting HTML will be
+plain.  Likewise, if you don't like the choice of colors, fix the mode
+that created them, or simply alter the faces it uses.
+
+\(fn &optional BUFFER)" t nil)
+
+(autoload 'htmlize-region "htmlize/htmlize" "\
+Convert the region to HTML, preserving colors and decorations.
+See `htmlize-buffer' for details.
+
+\(fn BEG END)" t nil)
+
+(autoload 'htmlize-file "htmlize/htmlize" "\
+Load FILE, fontify it, convert it to HTML, and save the result.
+
+Contents of FILE are inserted into a temporary buffer, whose major mode
+is set with `normal-mode' as appropriate for the file type.  The buffer
+is subsequently fontified with `font-lock' and converted to HTML.  Note
+that, unlike `htmlize-buffer', this function explicitly turns on
+font-lock.  If a form of highlighting other than font-lock is desired,
+please use `htmlize-buffer' directly on buffers so highlighted.
+
+Buffers currently visiting FILE are unaffected by this function.  The
+function does not change current buffer or move the point.
+
+If TARGET is specified and names a directory, the resulting file will be
+saved there instead of to FILE's directory.  If TARGET is specified and
+does not name a directory, it will be used as output file name.
+
+\(fn FILE &optional TARGET)" t nil)
+
+(autoload 'htmlize-many-files "htmlize/htmlize" "\
+Convert FILES to HTML and save the corresponding HTML versions.
+
+FILES should be a list of file names to convert.  This function calls
+`htmlize-file' on each file; see that function for details.  When
+invoked interactively, you are prompted for a list of files to convert,
+terminated with RET.
+
+If TARGET-DIRECTORY is specified, the HTML files will be saved to that
+directory.  Normally, each HTML file is saved to the directory of the
+corresponding source file.
+
+\(fn FILES &optional TARGET-DIRECTORY)" t nil)
+
+(autoload 'htmlize-many-files-dired "htmlize/htmlize" "\
+HTMLize dired-marked files.
+
+\(fn ARG &optional TARGET-DIRECTORY)" t nil)
+
+;;;***
+
+;;;### (autoloads (turn-on-hungry-delete-mode hungry-delete-mode
+;;;;;;  hungry-delete-backward hungry-delete-forward) "hungury-delete/hungry-delete"
+;;;;;;  "hungury-delete/hungry-delete.el" (19775 63823))
+;;; Generated autoloads from hungury-delete/hungry-delete.el
+
+(autoload 'hungry-delete-forward "hungury-delete/hungry-delete" "\
+Delete the following character or all following whitespace up
+to the next non-whitespace character.  See
+\\[c-hungry-delete-backward].
+
+\(fn)" t nil)
+
+(autoload 'hungry-delete-backward "hungury-delete/hungry-delete" "\
+Delete the preceding character or all preceding whitespace
+back to the previous non-whitespace character.  See also
+\\[c-hungry-delete-forward].
+
+\(fn)" t nil)
+
+(autoload 'hungry-delete-mode "hungury-delete/hungry-delete" "\
+Minor mode to enable hungry deletion.  This will delete all
+whitespace after or before point when the deletion command is
+executed.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'turn-on-hungry-delete-mode "hungury-delete/hungry-delete" "\
+Turns on hungry delete mode if the buffer is appropriate.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads (lusty-launch-dired lusty-select-current-name lusty-select-match
+;;;;;;  lusty-open-this lusty-highlight-previous-column lusty-highlight-next-column
+;;;;;;  lusty-highlight-previous lusty-highlight-next lusty-buffer-explorer
+;;;;;;  lusty-file-explorer) "lusty-explorer/lusty-explorer" "lusty-explorer/lusty-explorer.el"
+;;;;;;  (19777 341))
+;;; Generated autoloads from lusty-explorer/lusty-explorer.el
+
+(autoload 'lusty-file-explorer "lusty-explorer/lusty-explorer" "\
+Launch the file/directory mode of LustyExplorer.
+
+\(fn)" t nil)
+
+(autoload 'lusty-buffer-explorer "lusty-explorer/lusty-explorer" "\
+Launch the buffer mode of LustyExplorer.
+
+\(fn)" t nil)
+
+(autoload 'lusty-highlight-next "lusty-explorer/lusty-explorer" "\
+Highlight the next match in *Lusty-Matches*.
+
+\(fn)" t nil)
+
+(autoload 'lusty-highlight-previous "lusty-explorer/lusty-explorer" "\
+Highlight the previous match in *Lusty-Matches*.
+
+\(fn)" t nil)
+
+(autoload 'lusty-highlight-next-column "lusty-explorer/lusty-explorer" "\
+Highlight the next column in *Lusty-Matches*.
+
+\(fn)" t nil)
+
+(autoload 'lusty-highlight-previous-column "lusty-explorer/lusty-explorer" "\
+Highlight the previous column in *Lusty-Matches*.
+
+\(fn)" t nil)
+
+(autoload 'lusty-open-this "lusty-explorer/lusty-explorer" "\
+Open the given file/directory/buffer, creating it if not already present.
+
+\(fn)" t nil)
+
+(autoload 'lusty-select-match "lusty-explorer/lusty-explorer" "\
+Activate the highlighted match in *Lusty-Matches* - recurse if dir, open if file/buffer.
+
+\(fn)" t nil)
+
+(autoload 'lusty-select-current-name "lusty-explorer/lusty-explorer" "\
+Open the given file/buffer or create a new buffer with the current name.
+
+\(fn)" t nil)
+
+(autoload 'lusty-launch-dired "lusty-explorer/lusty-explorer" "\
+Launch dired at the current directory.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (multi-term) "multi-term/multi-term" "multi-term/multi-term.el"
+;;;;;;  (19777 341))
+;;; Generated autoloads from multi-term/multi-term.el
+
+(autoload 'multi-term "multi-term/multi-term" "\
+Create new term buffer.
+Will prompt you shell name when you type `C-u' before this command.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (nav) "nav/nav" "nav/nav.el" (19777 35651))
+;;; Generated autoloads from nav/nav.el
+
+(autoload 'nav "nav/nav" "\
+Run nav-mode in a narrow window on the left side.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (pylookup-update pylookup-lookup) "pylookup/pylookup"
+;;;;;;  "pylookup/pylookup.el" (19775 58061))
+;;; Generated autoloads from pylookup/pylookup.el
+
+(autoload 'pylookup-lookup "pylookup/pylookup" "\
+Lookup SEARCH-TERM in the Python HTML indexes.
+
+\(fn SEARCH-TERM)" t nil)
+
+(autoload 'pylookup-update "pylookup/pylookup" "\
+Run pylookup-update and create the database at `pylookup-db-file'.
+
+\(fn SRC)" t nil)
+
+;;;***
+
+;;;### (autoloads (rainbow-mode) "rainbow-mode/rainbow-mode" "rainbow-mode/rainbow-mode.el"
+;;;;;;  (19777 341))
+;;; Generated autoloads from rainbow-mode/rainbow-mode.el
+
+(autoload 'rainbow-mode "rainbow-mode/rainbow-mode" "\
+Colorize strings that represent colors.
+This will fontify with colors the string like \"#aabbcc\" or \"blue\".
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (resume save-current-configuration wipe restore-window-configuration
+;;;;;;  current-window-configuration-printable) "revive/revive" "revive/revive.el"
+;;;;;;  (19777 35651))
+;;; Generated autoloads from revive/revive.el
+
+(autoload 'current-window-configuration-printable "revive/revive" "\
+Return the printable current-window-configuration.
+This configuration will be stored by restore-window-configuration.
+Returned configurations are list of:
+'(Screen-Width Screen-Height Edge-List Buffer-List)
+
+Edge-List is a return value of revive:all-window-edges, list of all
+window-edges whose first member is always of north west window.
+
+Buffer-List is a list of buffer property list of all windows.  This
+property lists are stored in order corresponding to Edge-List.  Buffer
+property list is formed as
+'((buffer-file-name) (buffer-name) (point) (window-start)).
+
+\(fn)" nil nil)
+
+(autoload 'restore-window-configuration "revive/revive" "\
+Restore the window configuration.
+Configuration CONFIG should be created by
+current-window-configuration-printable.
+
+\(fn CONFIG)" nil nil)
+
+(autoload 'wipe "revive/revive" "\
+Wipe Emacs.
+
+\(fn)" t nil)
+
+(autoload 'save-current-configuration "revive/revive" "\
+Save current window/buffer configuration into configuration file.
+
+\(fn &optional NUM)" t nil)
+
+(autoload 'resume "revive/revive" "\
+Resume window/buffer configuration.
+Configuration should be saved by save-current-configuration.
+
+\(fn &optional NUM)" t nil)
+
+;;;***
+
+;;;### (autoloads (sr-dired sunrise-cd sunrise) "sunrise-commander/sunrise-commander"
+;;;;;;  "sunrise-commander/sunrise-commander.el" (19777 35651))
+;;; Generated autoloads from sunrise-commander/sunrise-commander.el
+
+(autoload 'sunrise "sunrise-commander/sunrise-commander" "\
+Starts the Sunrise Commander. If the param `left-directory' is given the left
+window  will  display  this  directory  (the  same   for   `right-directory').
+Specifying nil for any of these values uses the default, ie. home.
+
+\(fn &optional LEFT-DIRECTORY RIGHT-DIRECTORY FILENAME)" t nil)
+
+(autoload 'sunrise-cd "sunrise-commander/sunrise-commander" "\
+Run Sunrise but give it the current directory to use.
+
+\(fn)" t nil)
+
+(autoload 'sr-dired "sunrise-commander/sunrise-commander" "\
+Visits the given directory in sr-mode.
+
+\(fn DIRECTORY &optional SWITCHES)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("async-eval/async-eval.el" "auto-async-byte-compile/auto-async-byte-compile.el"
+;;;;;;  "auto-complete-clang/auto-complete-clang.el" "ppindent/ppindent.el"
+;;;;;;  "pymacs/pymacs.el" "ssh-config/ssh-config.el") (19783 56171
+;;;;;;  669826))
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
