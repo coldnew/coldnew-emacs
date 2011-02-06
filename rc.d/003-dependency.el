b5b75bb03f733f01398e7993c2485867391b0834
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-06 17:09:39 +0800

    rename require-maybe macro to require*

--- rc.d/003-dependency.el
+++ rc.d/003-dependency.el
@@ -4,120 +4,120 @@
 
 
 
-(require-maybe 'ac-anything)			; Auto Complete with Anything
-(require-maybe 'ac-company)			; Auto-Complete with company-mode
-(require-maybe 'advice)				;
-(require-maybe 'ansi-color)			;
-(require-maybe 'anything)			;
-(require-maybe 'anything-c-shell-history)	; shell history anything.el interface
-(require-maybe 'anything-complete)		;
-(require-maybe 'anything-config)		;
-(require-maybe 'anything-dabbrev-expand)	; dabbrev using anything.el
-(require-maybe 'anything-grep)			; search refinement of grep result with anything
-(require-maybe 'anything-gtags)			;
-(require-maybe 'anything-include)		; Anything-source made maintenance of history of C/C++
-(require-maybe 'anything-ipa)			;
-(require-maybe 'anything-kyr)			; Show context-aware commands
-(require-maybe 'anything-match-plugin)		;
-(require-maybe 'anything-menu)			; anything.el candidate selection outside Emacs
-(require-maybe 'anything-migemo)		;
-(require-maybe 'anything-show-completion)	; Show selection in buffer for anything completion
-(require-maybe 'anything-slime)			; anything-sources and some utilities for SLIME.
-(require-maybe 'anything-yaetags)		; Yet another etags interface with anything
-(require-maybe 'async-eval)			; execute Emacs lisp in a separate process
-(require-maybe 'auto-complete)			; Auto-Complete
-(require-maybe 'auto-complete-clang)		;
-(require-maybe 'auto-complete-config)		;
-(require-maybe 'auto-complete-etags)		;
-(require-maybe 'auto-complete-extension)	;
-(require-maybe 'auto-dictionary)		;
-(require-maybe 'c-eldoc)			;
-(require-maybe 'cc-mode)			;
-(require-maybe 'cedet)				;
-(require-maybe 'cflow-mode)			; major mode for viewing cflow output files
-(require-maybe 'cl)				;
-(require-maybe 'color-theme)			; Color-theme
-(require-maybe 'company)			; Another aoto-complete plugins
-(require-maybe 'completion-ui)			;
-(require-maybe 'csharp-mode)			; C# mode derived mode
-(require-maybe 'ctagsfind)			;
-(require-maybe 'dabbrev)			;
-(require-maybe 'desktop)			;
-(require-maybe 'doxymacs)			; Doxygen for emacs
-(require-maybe 'dropdown-list)			;
-(require-maybe 'el-get)				; Emacs-Lisp Manager
-(require-maybe 'eldoc)				; Emacs-Lisp Documents Browser
-(require-maybe 'eldoc-extension)		; Emacs-Lisp Documents Browser Extension
-(require-maybe 'emms)				; Emacs Multimedia System
-(require-maybe 'etags)				;
-(require-maybe 'executable)			;
-(require-maybe 'find-file)			;
-(require-maybe 'flyspell)			; on-the-fly spell checker
-(require-maybe 'gccsense)			; GCC's code analyzers
-(require-maybe 'gnugo)				; Run GNU Go in a buffer
-(require-maybe 'gobject-class)			; functions to easy GObject-based class developers
-(require-maybe 'grep)				;
-(require-maybe 'highlight-cl)			;
-(require-maybe 'highlight-parentheses)		; Highlight pair parentheses
-(require-maybe 'htmlize)			;
-(require-maybe 'hungry-delete)			;
-(require-maybe 'ibuf-ext)			; iBuffer Extension
-(require-maybe 'ibuffer)			; iBuffer
-(require-maybe 'idutils)			;
-(require-maybe 'info)				;
-(require-maybe 'ipa)				; In-place annotations
-(require-maybe 'linum)				;
-(require-maybe 'lusty-explorer)			; A good explorer tools
-(require-maybe 'magit)				;
-(require-maybe 'midnight)			; Use midnight-mode to auto-clean buffers
-(require-maybe 'multi-term)			;
-(require-maybe 'nav)				;
-(require-maybe 'newlisp)			; newlisp
-(require-maybe 'paredit)			; Use Paredit to balance parentheses
-(require-maybe 'popup-pos-tip)			;
-(require-maybe 'pos-tip)			;
-(require-maybe 'pp)				; pretty printer for Emacs Lisp
-(require-maybe 'ppindent)			;
-(require-maybe 'pretty-lambdada)		; Show lambda in smbol
-(require-maybe 'python)				; Python
-(require-maybe 'qmake-mode)			; qmake mode for emacs
-(require-maybe 'quack)				; enhanced support for editing and running Scheme code
-(require-maybe 'rainbow-mode)			; Color the emacs buffer
-(require-maybe 'rcirc)				;
-(require-maybe 'rcirc-color)			; color rcirc nicks
-(require-maybe 'rcirc-controls)			; color rcirc control sequences
-(require-maybe 'recentf)			; Setup a menu of recently opened files
-(require-maybe 'rw-hunspell)			;
-(require-maybe 'rw-ispell)			;
-(require-maybe 'rw-language-and-country-codes)	;
-(require-maybe 'session)			; Use Session to save current positions
-(require-maybe 'shell)				;
-(require-maybe 'shell-pop)			; Helps you pop up and pop out shell buffer easily
-(require-maybe 'slime)				; Superior Lisp Interaction Mode for Emacs
-(require-maybe 'smartchr)			; Emacs version of smartchr.vim
-(require-maybe 'speck)				; minor mode for spell checking
-(require-maybe 'ssh-config)			; Syntax Highlight ssh-configure files
-(require-maybe 'sunrise-commander)		; A commander looks like MC
-(require-maybe 'term)				;
-(require-maybe 'thingatpt)			; Get the thing at point
-(require-maybe 'undo-tree)			; Use undo-tree instead of redo.el
-(require-maybe 'unicad)				; Universal Charset Auto Detector
-(require-maybe 'uniquify)			; If two buffer have the same name, rename both
-(require-maybe 'vim)				; Use Vim-Mode
-(require-maybe 'w3m)				;
-(require-maybe 'w3m-lnum)			;
-(require-maybe 'windmove)			; Directional window-selection routines
-(require-maybe 'windows)			; Window manager for GNU Emacs.
-(require-maybe 'woman)				; Use Woman
-(require-maybe 'x-dnd)				; Drag and drop support for X
-(require-maybe 'xcscope)			; Cscope interface for Emacs
-(require-maybe 'xcscope+)			; Providing an extension to xcscope
-(require-maybe 'yasnippet)			; Yet another snippet extension for Emacs
-(require-maybe 'zencoding-mode)			; Unfold CSS-selector-like expressions to markup
+(require* 'ac-anything)			  ; Auto Complete with Anything
+(require* 'ac-company)			  ; Auto-Complete with company-mode
+(require* 'advice)			  ;
+(require* 'ansi-color)			  ;
+(require* 'anything)			  ;
+(require* 'anything-c-shell-history)	  ; shell history anything.el interface
+(require* 'anything-complete)		  ;
+(require* 'anything-config)		  ;
+(require* 'anything-dabbrev-expand)	  ; dabbrev using anything.el
+(require* 'anything-grep)		  ; search refinement of grep result with anything
+(require* 'anything-gtags)		  ;
+(require* 'anything-include)		  ; Anything-source made maintenance of history of C/C++
+(require* 'anything-ipa)		  ;
+(require* 'anything-kyr)		  ; Show context-aware commands
+(require* 'anything-match-plugin)	  ;
+(require* 'anything-menu)		  ; anything.el candidate selection outside Emacs
+(require* 'anything-migemo)		  ;
+(require* 'anything-show-completion)	  ; Show selection in buffer for anything completion
+(require* 'anything-slime)		  ; anything-sources and some utilities for SLIME.
+(require* 'anything-yaetags)		  ; Yet another etags interface with anything
+(require* 'async-eval)			  ; execute Emacs lisp in a separate process
+(require* 'auto-complete)		  ; Auto-Complete
+(require* 'auto-complete-clang)		  ;
+(require* 'auto-complete-config)	  ;
+(require* 'auto-complete-etags)		  ;
+(require* 'auto-complete-extension)	  ;
+(require* 'auto-dictionary)		  ;
+(require* 'c-eldoc)			  ;
+(require* 'cc-mode)			  ;
+(require* 'cedet)			  ;
+(require* 'cflow-mode)			  ; major mode for viewing cflow output files
+(require* 'cl)				  ;
+(require* 'color-theme)			  ; Color-theme
+(require* 'company)			  ; Another aoto-complete plugins
+(require* 'completion-ui)		  ;
+(require* 'csharp-mode)			  ; C# mode derived mode
+(require* 'ctagsfind)			  ;
+(require* 'dabbrev)			  ;
+(require* 'desktop)			  ;
+(require* 'doxymacs)			  ; Doxygen for emacs
+(require* 'dropdown-list)		  ;
+(require* 'el-get)			  ; Emacs-Lisp Manager
+(require* 'eldoc)			  ; Emacs-Lisp Documents Browser
+(require* 'eldoc-extension)		  ; Emacs-Lisp Documents Browser Extension
+(require* 'emms)			  ; emacs Multimedia System
+(require* 'etags)			  ;
+(require* 'executable)			  ;
+(require* 'find-file)			  ;
+(require* 'flyspell)			  ; on-the-fly spell checker
+(require* 'gccsense)			  ; GCC's code analyzers
+(require* 'gnugo)			  ; Run GNU Go in a buffer
+(require* 'gobject-class)		  ; functions to easy GObject-based class developers
+(require* 'grep)			  ;
+(require* 'highlight-cl)		  ;
+(require* 'highlight-parentheses)	  ; Highlight pair parentheses
+(require* 'htmlize)			  ;
+(require* 'hungry-delete)		  ;
+(require* 'ibuf-ext)			  ; iBuffer Extension
+(require* 'ibuffer)			  ; iBuffer
+(require* 'idutils)			  ;
+(require* 'info)			  ;
+(require* 'ipa)				  ; In-place annotations
+(require* 'linum)			  ;
+(require* 'lusty-explorer)		  ; A good explorer tools
+(require* 'magit)			  ;
+(require* 'midnight)			  ; Use midnight-mode to auto-clean buffers
+(require* 'multi-term)			  ;
+(require* 'nav)				  ;
+(require* 'newlisp)			  ; newlisp
+(require* 'paredit)			  ; Use Paredit to balance parentheses
+(require* 'popup-pos-tip)		  ;
+(require* 'pos-tip)			  ;
+(require* 'pp)				  ; pretty printer for Emacs Lisp
+(require* 'ppindent)			  ;
+(require* 'pretty-lambdada)		  ; Show lambda in smbol
+(require* 'python)			  ; Python
+(require* 'qmake-mode)			  ; qmake mode for emacs
+(require* 'quack)			  ; enhanced support for editing and running Scheme code
+(require* 'rainbow-mode)		  ; Color the emacs buffer
+(require* 'rcirc)			  ;
+(require* 'rcirc-color)			  ; color rcirc nicks
+(require* 'rcirc-controls)		  ; color rcirc control sequences
+(require* 'recentf)			  ; Setup a menu of recently opened files
+(require* 'rw-hunspell)			  ;
+(require* 'rw-ispell)			  ;
+(require* 'rw-language-and-country-codes) ;
+(require* 'session)			  ; Use Session to save current positions
+(require* 'shell)			  ;
+(require* 'shell-pop)			  ; Helps you pop up and pop out shell buffer easily
+(require* 'slime)			  ; Superior Lisp Interaction Mode for Emacs
+(require* 'smartchr)			  ; Emacs version of smartchr.vim
+(require* 'speck)			  ; minor mode for spell checking
+(require* 'ssh-config)			  ; Syntax Highlight ssh-configure files
+(require* 'sunrise-commander)		  ; A commander looks like MC
+(require* 'term)			  ;
+(require* 'thingatpt)			  ; Get the thing at point
+(require* 'undo-tree)			  ; Use undo-tree instead of redo.el
+(require* 'unicad)			  ; Universal Charset Auto Detector
+(require* 'uniquify)			  ; If two buffer have the same name, rename both
+(require* 'vim)				  ; Use Vim-Mode
+(require* 'w3m)				  ;
+(require* 'w3m-lnum)			  ;
+(require* 'windmove)			  ; Directional window-selection routines
+(require* 'windows)			  ; Window manager for GNU Emacs.
+(require* 'woman)			  ; Use Woman
+(require* 'x-dnd)			  ; Drag and drop support for X
+(require* 'xcscope)			  ; Cscope interface for Emacs
+(require* 'xcscope+)			  ; Providing an extension to xcscope
+(require* 'yasnippet)			  ; Yet another snippet extension for Emacs
+(require* 'zencoding-mode)		  ; Unfold CSS-selector-like expressions to markup
 
-;;(require-maybe 'ipython)			; iPython
-;;(require-maybe 'matlab)			; Matlab
-;;(require-maybe 'pymacs)			; Pymacs
+;;(require* 'ipython)			; iPython
+;;(require* 'matlab)			; Matlab
+;;(require* 'pymacs)			; Pymacs
 
 
 
