4b27220ec7f3c5feeea527b6829b6d9409ee5e20
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-08 17:44:05 +0800

    add

---
+++ rc.d/051-tramp.el
@@ -0,0 +1,11 @@
+;;
+(eval-when-compile (require 'cl))
+
+(setq tramp-default-method "ssh")
+(setq tramp-persistency-file-name "~/.emacs.d/var/cache/tramp.cache")
+
+
+
+
+(provide '051-tramp)
+;; 051-tramp.el ends here.
