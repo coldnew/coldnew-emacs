a666b36eeda3e6014b5b0e3778f68ff5e248b425
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-14 22:35:27 +0800

    remodify init.el

--- init.el
+++ init.el
@@ -23,39 +23,7 @@
 	(let* ((feature (file-name-sans-extension (file-name-nondirectory config-file)))
 	       (loading-result (require (intern feature) nil 'noerror)))
 	  (with-current-buffer (get-buffer-create "*Loading Config Log*")
-	    (insert (format "\t Loading %-20s\t \n" (concat init-folder feature ".el")))))))
+	    (insert (format "\t Loading %-20s\t \n" (concat init-folder config-file)))))))
   ;; After loading all my config file, read my authorization file
   (if (file-exists-p authinfo-file) (load-file authinfo-file))
   (message "\t --- Loading All Emacs Configures Finish ---"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auto-compression-mode t)
- '(blink-cursor-mode nil)
- '(custom-file nil)
- '(global-auto-revert-mode t)
- '(global-font-lock-mode t)
- '(gnus-inhibit-startup-message t)
- '(inhibit-default-init t)
- '(inhibit-startup-screen t)
- '(initial-scratch-message "")
- '(kill-whole-line t)
- '(major-mode (quote lisp-interaction-mode))
- '(menu-bar-mode t)
- '(mouse-avoidance-mode (quote animate) nil (avoid))
- '(mouse-yank-at-point t)
- '(scroll-bar-mode nil)
- '(show-paren-mode t)
- '(tool-bar-mode nil)
- '(track-eol t)
- '(transient-mark-mode t)
- '(visible-bell t)
- '(x-select-enable-clipboard t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- lisp/.loaddefs.el
+++ lisp/.loaddefs.el
@@ -2537,6 +2537,57 @@ Note that this only works if the opening tag starts at column 0.
 
 ;;;***
 
+;;;### (autoloads (egg-minor-mode-find-file-hook egg-minor-mode)
+;;;;;;  "egg/egg" "egg/egg.el" (19801 12627))
+;;; Generated autoloads from egg/egg.el
+
+(autoload 'egg-minor-mode "egg/egg" "\
+Turn-on egg-minor-mode which would enable key bindings for
+egg in current buffer.\\<egg-minor-mode-map>
+\\[egg-start-new-branch] start a new branch from the current HEAD.
+\\[egg-status] shows the repo's current status
+\\[egg-commit-log-edit] start editing the commit message for the current staged changes.
+\\[egg-file-stage-current-file] stage new changes of the current file
+\\[egg-log] shows repo's history
+\\[egg-file-checkout-other-version] checkout another version of the current file
+\\[egg-file-cancel-modifications] delete unstaged modifications in the current file
+\\[egg-next-action] perform the next logical action
+\\[egg-file-diff] compare file with index or other commits
+\\[egg-file-version-other-window] show other version of the current file.
+
+\\{egg-minor-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'egg-minor-mode-find-file-hook "egg/egg" "\
+Not documented
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads (egg-grep egg-grep-mode egg-grep-process-setup)
+;;;;;;  "egg/egg-grep" "egg/egg-grep.el" (19801 12627))
+;;; Generated autoloads from egg/egg-grep.el
+
+(autoload 'egg-grep-process-setup "egg/egg-grep" "\
+Setup compilation variables and buffer for `egg-grep'.
+Set up `compilation-exit-message-function' and run `egg-grep-setup-hook'.
+
+\(fn)" nil nil)
+
+(autoload 'egg-grep-mode "egg/egg-grep" "\
+Sets `compilation-last-buffer' and `compilation-window-height'.
+
+\(fn)" nil nil)
+
+(autoload 'egg-grep "egg/egg-grep" "\
+Not documented
+
+\(fn LEVEL)" t nil)
+
+;;;***
+
 ;;;### (autoloads (bookmark-w3m-bookmark-jump) "emacs-w3m/bookmark-w3m"
 ;;;;;;  "emacs-w3m/bookmark-w3m.el" (19796 35698))
 ;;; Generated autoloads from emacs-w3m/bookmark-w3m.el
@@ -3542,6 +3593,24 @@ Default MODIFIER is 'meta.
 
 ;;;***
 
+;;;### (autoloads (git-reblame git-blame-mode) "git-emacs/git-blame"
+;;;;;;  "git-emacs/git-blame.el" (19801 12864))
+;;; Generated autoloads from git-emacs/git-blame.el
+
+(autoload 'git-blame-mode "git-emacs/git-blame" "\
+Toggle minor mode for displaying Git blame
+
+With prefix ARG, turn the mode on if ARG is positive.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'git-reblame "git-emacs/git-blame" "\
+Recalculate all blame information in the current buffer
+
+\(fn)" t nil)
+
+;;;***
+
 ;;;### (autoloads (gnugo) "gnugo/gnugo" "gnugo/gnugo.el" (19796 35696))
 ;;; Generated autoloads from gnugo/gnugo.el
 
@@ -4330,8 +4399,10 @@ accept it or skip it.
 
 ;;;***
 
-;;;### (autoloads nil nil ("layout-restore/layout-restore.el" "redo+/redo+.el")
-;;;;;;  (19798 44504 859925))
+;;;### (autoloads nil nil ("git-emacs/git--test.el" "git-emacs/git-emacs-autoloads.el"
+;;;;;;  "git-emacs/git-emacs.el" "git-emacs/git-global-keys.el" "git-emacs/git-log.el"
+;;;;;;  "git-emacs/git-modeline.el" "git-emacs/git-status.el" "redo+/redo+.el")
+;;;;;;  (19801 12864 856542))
 
 ;;;***
 
