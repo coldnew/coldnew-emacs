06a7ac8c1e1775159aefb40b078faacb6bda13f5
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-14 23:57:09 +0800

    update 054-egg.el

--- rc.d/054-egg.el
+++ rc.d/054-egg.el
@@ -7,7 +7,7 @@
 (setq egg-auto-update t)
 
 ;; do not switch to the status buffer in the same window
-(setq egg-switch-to-buffer nil)
+(setq egg-switch-to-buffer t)
 
 ;; kyebindings
 (add-hook 'egg-status-buffer-mode-hook
@@ -18,11 +18,21 @@
 	     (vim:local-nmap (kbd "p") 'egg-buffer-cmd-navigate-prev)
 	     (vim:local-nmap (kbd "n") 'egg-buffer-cmd-navigate-next)
 	     (vim:local-nmap (kbd "l") 'egg-log)
-	     (vim:local-nmap (kbd "g") 'egg-status-buffer-redisplay)
-
+	     (vim:local-nmap (kbd "G") 'egg-status-buffer-redisplay)
+	     (vim:local-nmap (kbd "h") 'egg-section-cmd-toggle-hide-show)
+	     (vim:local-nmap (kbd "H") 'egg-section-cmd-toggle-hide-show-children)
+	     (vim:local-nmap (kbd "h") 'egg-quit-buffer)
+	     (vim:local-nmap (kbd "u") 'egg-diff-section-cmd-undo)
 	     ))
 
-;;(define-key egg-buffer-mode-map)
+;;;;;;;; Advice
+
+;; visiting a file in ibuffer makes it "fullscreen"
+(defadvice egg-status (around goto-egg-status-buffer activate)
+  "Delete other windows after visiting egg-status"
+  ad-do-it
+  (delete-other-windows))
+
 
 (provide '054-egg)
 ;; 054-egg.el ends here.
