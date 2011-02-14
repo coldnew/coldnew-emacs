20a26337b1a769f4b84c559487b2ecd270a458ac
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-14 22:29:11 +0800

    add 054-egg.el

---
+++ rc.d/054-egg.el
@@ -0,0 +1,14 @@
+;;
+(eval-when-compile (require 'cl))
+
+
+
+;; auto-update egg-status on file save
+(setq egg-auto-update t)
+
+;; switch to the status buffer in the same window
+(setq egg-switch-to-buffer t)
+
+
+(provide '054-egg)
+;; 054-egg.el ends here.
