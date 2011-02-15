4f115f2160c28d79e3dcd7391d40ac5f09c2474d
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-15 13:53:27 +0800

    add lang-spice config to use spice-mode

--- rc.d/lang-spice.el
+++ rc.d/lang-spice.el
@@ -1,9 +1,19 @@
 ;;
 (eval-when-compile (require 'cl))
 
+;;;;;;;; Variable Setting
+;; Use ng-spice-rework as spice simulator
+(setq spice-simulator "ngspice")
+;; Empty initial file
+(setq spice-initialize-empty-file t)
+;; Highlight keywords
+(setq spice-highlight-keywords t)
+;; Describe mode at startup
+(setq spice-show-describe-mode t)
 
 
-;; Auto-mode
+
+;;;;;;;; Auto-mode
 (add-to-list 'auto-mode-alist '("\\.cir\\'" . spice-mode))
 (add-to-list 'auto-mode-alist '("\\.sp\\'"  . spice-mode))
 (add-to-list 'auto-mode-alist '("\\.spice\\'"  . spice-mode))
