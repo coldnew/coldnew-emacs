7b15d15b9442d43ae269b78339dfd6b0ae4cf1f6
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-16 14:39:01 +0800

    fix function

--- rc.d/054-egg.el
+++ rc.d/054-egg.el
@@ -61,14 +61,12 @@
 (defadvice egg-status (around goto-egg-status-buffer activate)
   "Delete other windows after visiting egg-status.
    Kill egg-status buffer after commit."
-  (kill-buffer)
   ad-do-it
   (delete-other-windows))
 
 (defadvice egg-commit-log-edit (around goto-egg-commit-buffer activate)
   "Delete other windows after visiting egg-commit-buffer.
    Kill eg-commit buffer after commit."
-  (kill-buffer)
   ad-do-it
   (delete-other-windows))
 
