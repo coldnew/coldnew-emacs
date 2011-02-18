2cc16a8bbe10096dd58a5ed9b9865d0adfc5f596
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-18 13:03:33 +0800

    make comint mode do not show password

--- rc.d/031-comint-mode.el
+++ rc.d/031-comint-mode.el
@@ -8,6 +8,10 @@
 	     (vim:local-imap (kbd "RET") 'comint-send-input)
 	     ))
 
+;; Do not show password
+(setq comint-output-filter-functions  '(comint-watch-for-password-prompt))
+(setq comint-password-prompt-regexp
+      "\\(\\([Oo]ld \\|[Nn]ew \\|^\\)[Pp]assword\\|Enter password\\):\\s *\\'")
 
 ;;;;;; Functions
 (defun comint-mode:clear-region ()
