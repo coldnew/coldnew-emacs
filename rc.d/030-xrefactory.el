9b10c53ced0fcf3618d7083d43f0f16a4776cc77
Yen-Chin, Lee <coldnew.tw@gmail.com>
2011-02-22 10:05:31 +0800

    comment all setting in xref

--- rc.d/030-xrefactory.el
+++ rc.d/030-xrefactory.el
@@ -1,12 +1,12 @@
 
-
-(cond
- (linux-p
-  (setq load-path (cons "~/.emacs.d/lisp/xref-linux/emacs" load-path))
-  (setq exec-path (cons "~/.emacs.d/lisp/xref" exec-path))
-  (require 'xrefactory)
-  )
- )
+;; FIXME: annoying
+;; (cond
+;;  (linux-p
+;;   (setq load-path (cons "~/.emacs.d/lisp/xref-linux/emacs" load-path))
+;;   (setq exec-path (cons "~/.emacs.d/lisp/xref" exec-path))
+;;   (require 'xrefactory)
+;;   )
+;;  )
 
 
 (provide '030-xrefactory)
