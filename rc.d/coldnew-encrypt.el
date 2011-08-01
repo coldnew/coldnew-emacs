7557ad8d3f347e5c742c136b9642dd0622f6d5b5
Yen-Chin,Lee <coldnew.tw@gmail.com>
2011-08-01 20:51:23 +0800

    update

---
+++ rc.d/coldnew-encrypt.el
@@ -0,0 +1,28 @@
+;;
+(eval-when-compile (require 'cl))
+
+;;;;;;;; Packages Import
+(require 'coldnew-macro)
+(require 'coldnew-functions)
+(require 'coldnew-variables)
+(require 'coldnew-commands)
+(require 'epa-file)
+
+;; Enable use EasyPG when attatch file which extension is .gpg
+(epa-file-enable)
+
+;; Select GnuPG Program
+(setq epg-gpg-program "/usr/bin/gpg")
+
+;; Always use symmetric encryption
+(setq epa-file-encrypt-to 'silent)
+
+;; save the password that only need type password once
+(setq epa-file-cache-passphrase-for-symmetric-encryption t)
+
+;; Enable auto-save in EasyPG
+(setq epa-file-inhibit-auto-save nil)
+
+
+(provide 'coldnew-encrypt)
+;; coldnew-encrypt.el ends here.
