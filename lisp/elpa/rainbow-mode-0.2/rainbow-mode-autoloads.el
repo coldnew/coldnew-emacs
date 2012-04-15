4c5abd97c56a0abfc75fe547752f3859d07907fa
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-04-15 23:45:59 +0800

    update

---
+++ lisp/elpa/rainbow-mode-0.2/rainbow-mode-autoloads.el
@@ -0,0 +1,13 @@
+;;; rainbow-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+(provide 'rainbow-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rainbow-mode-autoloads.el ends here
