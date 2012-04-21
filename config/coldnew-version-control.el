e06f88809de2025575a449af992ec7922a76cb5d
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-04-21 22:49:39 +0800

    add magit in coldnew-version-control.el

---
+++ config/coldnew-version-control.el
@@ -0,0 +1,12 @@
+;;; coldnew-version-control.el ---
+(eval-when-compile (require 'cl))
+
+;;;; ---------------------------------------------------------------------------
+;;;; Git
+;;;; ---------------------------------------------------------------------------
+(require 'magit)
+
+
+
+(provide 'coldnew-version-control)
+;; coldnew-version-control.el ends here.
