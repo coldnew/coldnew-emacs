cfceea8a5ad49244e0ad231bda3ccae13689cb90
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-04-26 04:34:56 +0800

    mask magit and git-emacs , I think I can try git-emacs next time.

--- config/coldnew-version-control.el
+++ config/coldnew-version-control.el
@@ -2,24 +2,19 @@
 (eval-when-compile (require 'cl))
 
 ;;;; ---------------------------------------------------------------------------
-;;;; Git
+;;;; Egg
 ;;;; ---------------------------------------------------------------------------
-
-;;;;;;;; Magit
-(require 'magit)
-
-;; if use magit, do not use vc-git to handle Git interface.
-;; (when (featurep 'magit)
-;;   (setq vc-handled-backends (remq 'Git vc-handled-backends)))
-
-;;;;;;;; Egg
 (require 'egg)
+
 ;; do not auto-update egg-status on file save
 (setq egg-auto-update nil)
+
 ;; do not switch to the status buffer in the same window
 (setq egg-switch-to-buffer t)
+
 ;; make egg auto guess next action
 (setq egg-confirm-next-action nil)
+
 ;; remodify next-action priority
 (defsubst egg-guess-next-action (desc)
   (cond
@@ -35,9 +30,12 @@
 
 
 
+;;;;;;;; Magit
+;;(require 'magit)
+;; if use magit, do not use vc-git to handle Git interface.
+;; (when (featurep 'magit)
+;;   (setq vc-handled-backends (remq 'Git vc-handled-backends)))
 ;; (require 'git-emacs)
-
-
 ;;   ;; disable git-emacs's advice
 ;;   (ad-disable-advice 'vc-next-action 'around 'git--vc-git-next-action)
 ;;   (ad-activate 'vc-next-action)
