e837674928b643431768992c43e287e23b58c79e
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-04-26 04:50:49 +0800

    update

--- config/coldnew-android.el
+++ config/coldnew-android.el
@@ -1,8 +1,10 @@
 ;;; coldnew-android.el --- android development
-;;; Time-stamp: <2012-04-25 20:55:52 (coldnew)>
+;;; Time-stamp: <2012-04-26 04:49:11 (coldnew)>
 (eval-when-compile (require 'cl))
 
 (require 'android-mode)
+
+
 
 ;; Set my android-emulator-path
 (setq android-mode-sdk-dir "/opt/android-sdk-update-manager/")
