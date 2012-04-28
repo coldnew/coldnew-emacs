b0487bf54df9cce5d968212c624614a5e4723378
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-04-28 22:22:53 +0800

    try to add mobileOrg to org-mode

--- config/coldnew-org.el
+++ config/coldnew-org.el
@@ -8,7 +8,10 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (add-to-list 'auto-mode-alist '("\\.org_archive$" . org-mode))
 
+;;;;;;;; Config
 
+;; Set MobileOrg file path
+;; (setq org-mobile-directroy "~/Dropbox/")
 
 
 (provide 'coldnew-org)
