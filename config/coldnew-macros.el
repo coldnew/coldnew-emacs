d71a3bfc0f8603ed0065a2d14640bdb818d6993e
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-01 21:29:22 +0800

    add require* macro

--- config/coldnew-macros.el
+++ config/coldnew-macros.el
@@ -1,6 +1,25 @@
 ;;; coldnew-macros.el --- some useful macros
 (eval-when-compile (require 'cl))
 
+(defmacro require* (feature &optional file)
+  "Try to require FEATURE, but don't signal an error if `reauire' fails.
+If this package does not exist, use el-get reinstall it."
+  `(let* ((require-result (require ,feature ,file 'noerror)))
+     ;; if package does not exist, reinstall it
+     (if-not require-result (el-get-reinstall ,feature)
+	     ;; TODO: add require-result buffer
+	     )))
+
+;; add font-lock
+(font-lock-add-keywords 'emacs-lisp-mode
+			'(("(\\(require\*\\*\\)\\s [ \t']*\\(\\sw+\\)?"
+			   (1 font-lock-keyword-face)
+			   (2 font-lock-constant-face nil t))))
+(font-lock-add-keywords 'lisp-interaction-mode
+			'(("(\\(require\*\\*\\)\\s [ \t']*\\(\\sw+\\)?"
+			   (1 font-lock-keyword-face)
+			   (2 font-lock-constant-face nil t))))
+
 ;;;; ---------------------------------------------------------------------------
 ;;;; Clojure
 ;;;; ---------------------------------------------------------------------------
