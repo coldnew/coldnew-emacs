11876ee68e905a6f203af2d7b6e88a68752fa703
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-01 16:34:52 +0800

    move depends to here

---
+++ config/coldnew-depends.el
@@ -0,0 +1,16 @@
+;;; coldnew-depends.el --- some packages wglobay use and without config wi put here.
+(eval-when-compile (require 'cl))
+
+
+(require 'ascii)
+(require 'ace-jump-mode)
+(require 'expand-region)
+(require 'unicad)
+(require 'iedit)
+
+
+
+
+
+(provide 'coldnew-depends)
+;; coldnew-depends.el ends here.
