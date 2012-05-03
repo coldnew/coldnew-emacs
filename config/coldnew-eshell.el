bdde451cbb54d9aac8a08b97b87d9775d7f9a508
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-04 01:45:20 +0800

    add prompt function

--- config/coldnew-eshell.el
+++ config/coldnew-eshell.el
@@ -1,7 +1,22 @@
 ;;; coldnew-eshell.el ---
 (eval-when-compile (require 'cl))
 
+(require 'eshell)
 
+;; ;;; Use ANSI color in eshell
+;; (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
+
+
+;;; Setup prompt function
+(setq eshell-prompt-function
+      '(lambda ()
+	 (concat
+	  user-login-name "@" system-name " "
+	  (if (string= (eshell/pwd) (directory-file-name (expand-file-name (getenv "HOME"))))
+	      "~"
+	    (eshell/pwd))
+	  (if (= (user-uid) 0) " # " " $ ")
+	  )))
 
 
 
