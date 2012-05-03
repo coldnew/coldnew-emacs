d495d52696c7f4dd42dbdbc3ec5ef08eabd9d19d
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-04 00:12:07 +0800

    add coldnew-elscreen

---
+++ config/coldnew-elscreen.el
@@ -0,0 +1,37 @@
+;;; coldnew-elscreen.el ---
+(eval-when-compile (require 'cl))
+
+(require* 'elscreen)
+
+;;; start elscreen
+(elscreen-start)
+
+;;; Make TAB show control ico
+(setq elscreen-tab-display-control t)
+
+;;; Make TAB do not display kill-screen icon
+(setq elscreen-tab-display-kill-screen nil)
+
+;;; Change elscreen prefix to
+(elscreen-set-prefix-key "\C-t")
+
+;;; Make elscreen create automatically
+(defmacro elscreen-create-automatically (ad-do-it)
+  (` (if (not (elscreen-one-screen-p))
+	 (, ad-do-it)
+       (elscreen-create)
+       (elscreen-notify-screen-modification 'force-immediately)
+       (elscreen-message "New screen is automatically created"))))
+
+(defadvice elscreen-next (around elscreen-create-automatically activate)
+  (elscreen-create-automatically ad-do-it))
+
+(defadvice elscreen-previous (around elscreen-create-automatically activate)
+  (elscreen-create-automatically ad-do-it))
+
+(defadvice elscreen-toggle (around elscreen-create-automatically activate)
+  (elscreen-create-automatically ad-do-it))
+
+
+(provide 'coldnew-elscreen)
+;; coldnew-elscreen.el ends here.
