2c4c7d1bbdb44435c8010cb69e00523520e33211
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-06 01:30:50 +0800

    add save buffer always to C-x C-s

--- config/coldnew-global-keybindings.el
+++ config/coldnew-global-keybindings.el
@@ -36,6 +36,7 @@
 (global-set-key (kbd "C-x M-x") 'anything-M-x)
 (global-set-key (kbd "C-x o")   'switch-window)
 (global-set-key (kbd "C-c C-f") 'my-anything-filelist)
+(global-set-key (kbd "C-c C-s") 'save-buffer-always)
 
 
 ;;;; ---------------------------------------------------------------------------
