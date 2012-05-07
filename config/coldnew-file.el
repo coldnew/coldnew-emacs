2bf807fff314c07ff6b08a392b77c2f4069d714c
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-08 00:23:06 +0800

    disable ido

--- config/coldnew-file.el
+++ config/coldnew-file.el
@@ -44,13 +44,13 @@
 ;;;; ---------------------------------------------------------------------------
 ;;;; ido
 ;;;; ---------------------------------------------------------------------------
-(require 'ido)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-(setq ido-use-filename-at-point 'guess)
-(setq ido-file-extensions-order '(".org" ".txt" ".py" ".emacs" ".xml" ".el" ".ini" ".cfg" ".cnf"))
-(setq ido-ignore-extensions t)
+;; (require 'ido)
+;; (setq ido-enable-flex-matching t)
+;; ;; (setq ido-everywhere t)
+;; ;; (ido-mode 1)
+;; (setq ido-use-filename-at-point 'guess)
+;; (setq ido-file-extensions-order '(".org" ".txt" ".py" ".emacs" ".xml" ".el" ".ini" ".cfg" ".cnf"))
+;; (setq ido-ignore-extensions t)
 
 
 ;;;; ---------------------------------------------------------------------------
