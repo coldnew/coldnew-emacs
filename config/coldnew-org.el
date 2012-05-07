00e3faeb0a02349ed485c2740db68bcd20ab328e
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-07 23:50:14 +0800

    make org-mdoe show everything default

--- config/coldnew-org.el
+++ config/coldnew-org.el
@@ -2,7 +2,7 @@
 (eval-when-compile (require 'cl))
 
 (require 'org-install)
-
+;;(require 'org-src)
 ;;;;;;;; org-mode extensions
 (add-to-list 'auto-mode-alist '("\\.txt$" . org-mode))
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -13,12 +13,14 @@
 ;; ;; Set MobileOrg file path
 ;; ;; (setq org-mobile-directroy "~/Dropbox/")
 
-
 (add-hook 'org-mode-hook
 	  '(lambda ()
 
 	     ;; do not show leading stars
 	     (setq org-hide-leading-stars t)
+
+	     ;; do not fold every content
+	     (setq org-startup-folded nil)
 
 	     ;; Latex Setting
 	     (setq org-latex-to-pdf-process
@@ -41,8 +43,27 @@
 ;;(add-hook 'org-mode-hook 'coldnew-editor-mode)
 
 ;;;; ---------------------------------------------------------------------------
+;;;; REMOVE HOOKS
+;;;; ---------------------------------------------------------------------------
+;; remove builtin-define org-mode keybindings, this will prevent conflit
+(add-hook 'org-mode-hook
+	  '(lambda ()
+	     (define-key org-mode-map (kbd "C-c C-f") nil)
+	     ))
+;;;; ---------------------------------------------------------------------------
 ;;;; Keybindings
 ;;;; ---------------------------------------------------------------------------
+(add-hook 'org-mode-hook
+	  '(lambda ()
+	     (local-set-key (kbd "C-c C-f") 'my-anything-filelist)
+	     (local-set-key (kbd "C-c C-c") 'org-edit-special)
+	     ))
+
+
+(add-hook 'org-src-mode-hook
+	  '(lambda ()
+	     (local-set-key (kbd "C-c C-c") 'org-edit-src-exit)
+	     ))
 
 (provide 'coldnew-org)
 ;; coldnew-org.el ends here.
