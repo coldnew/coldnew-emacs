386e3348783c2c8af09efd79709b070411d55da2
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-07 22:11:08 +0800

    add my old-config

--- config/coldnew-org.el
+++ config/coldnew-org.el
@@ -3,49 +3,46 @@
 
 (require 'org-install)
 
-;; ;;;;;;;; org-mode extensions
-;; (add-to-list 'auto-mode-alist '("\\.txt$" . org-mode))
-;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-;; (add-to-list 'auto-mode-alist '("\\.org_archive$" . org-mode))
+;;;;;;;; org-mode extensions
+(add-to-list 'auto-mode-alist '("\\.txt$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org_archive$" . org-mode))
 
 ;; ;;;;;;;; Config
 
 ;; ;; Set MobileOrg file path
 ;; ;; (setq org-mobile-directroy "~/Dropbox/")
 
-;; ;;; do not show leading stars
-;; (setq org-hide-leading-stars t)
 
-;; ;;; Latex settings
-;; (setq org-latex-to-pdf-process
-;;       '("xelatex -interaction nonstopmode -shell-escape %f"
-;;	"xelatex -interaction nonstopmode -shell-escape %f"))
+(add-hook 'org-mode-hook
+	  '(lambda ()
 
-;; (setq org-src-window-setup 'current-window)
+	     ;; do not show leading stars
+	     (setq org-hide-leading-stars t)
 
-;; (setq org-src-fontify-natively t)
+	     ;; Latex Setting
+	     (setq org-latex-to-pdf-process
+		   '("xelatex -interaction nonstopmode -shell-escape %f"
+		     "xelatex -interaction nonstopmode -shell-escape %f"))
 
-;; ;;;; ---------------------------------------------------------------------------
-;; ;;;; Hooks
-;; ;;;; ---------------------------------------------------------------------------
+	     ;;(setq org-latex-to-pdf-process '("texi2dvi --pdf --clean --verbose --batch %f"))
+	     (setq org-src-window-setup 'current-window)
 
+	     (setq org-src-fontify-natively t)
+	     ))
+
+
+;;;; ---------------------------------------------------------------------------
+;;;; Hooks
+;;;; ---------------------------------------------------------------------------
+
+;;; FIXME: coldnew-editor-mode will cause error with org-mode
 ;; ;;; use coldnew-editor-mode
-;; (add-hook 'org-mode-hook 'coldnew-editor-mode)
+;;(add-hook 'org-mode-hook 'coldnew-editor-mode)
 
-;; ;;;; ---------------------------------------------------------------------------
-;; ;;;; Keybindings
-;; ;;;; ---------------------------------------------------------------------------
-;; ;; Make windmove work in org-mode:
-;; (add-hook 'org-shiftup-final-hook 'windmove-up)
-;; (add-hook 'org-shiftleft-final-hook 'windmove-left)
-;; (add-hook 'org-shiftdown-final-hook 'windmove-down)
-;; (add-hook 'org-shiftright-final-hook 'windmove-right)
-
-;; (add-hook 'org-mode-hook
-;;	  (lambda ()
-;;	    (org-set-local 'yas/trigger-key [tab])
-;;	    (define-key yas/keymap [tab] 'yas/next-field-or-maybe-expand)))
-
+;;;; ---------------------------------------------------------------------------
+;;;; Keybindings
+;;;; ---------------------------------------------------------------------------
 
 (provide 'coldnew-org)
 ;; coldnew-org.el ends here.
