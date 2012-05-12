72695b163f3a5bf9963e78253415230c208e861d
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-13 00:07:01 +0800

    change popup-shell to eshell

--- config/coldnew-terminal.el
+++ config/coldnew-terminal.el
@@ -8,8 +8,8 @@
 ;; reset ansi-color
 (setq ansi-color-names-vector
       (vector (frame-parameter nil 'foreground-color)
-	      "#0B0B0E" "#CA3839" "#8ae234" "#edd400"
-	      "#729fcf" "#ad7fa8" "cyan3"   "#DCDCDC")
+          "#0B0B0E" "#CA3839" "#8ae234" "#edd400"
+          "#729fcf" "#ad7fa8" "cyan3"   "#DCDCDC")
       ansi-term-color-vector ansi-color-names-vector
       ansi-color-map (ansi-color-make-color-map))
 
@@ -17,7 +17,8 @@
 ;;;; shell-pop
 ;;;; ---------------------------------------------------------------------------
 (require 'shell-pop)
-(shell-pop-set-internal-mode "ansi-term")
+;;(shell-pop-set-internal-mode "ansi-term")
+(shell-pop-set-internal-mode "eshell")
 (shell-pop-set-internal-mode-shell emacs-default-shell)
 (shell-pop-set-window-height emacs-popup-shell-window-height)
 (shell-pop-set-window-position emacs-popup-shell-window-position)
@@ -25,11 +26,11 @@
 (defadvice shell-pop (before kill-dead-term activate)
   "If there is a stopped ansi-term, kill it and create a new one."
   (let ((running-p (term-check-proc (buffer-name)))
-	(term-p (string= "term-mode" major-mode)))
+    (term-p (string= "term-mode" major-mode)))
     (if term-p
-	(when (not running-p)
-	  (kill-buffer (buffer-name))
-	  (shell-pop-out)))))
+    (when (not running-p)
+      (kill-buffer (buffer-name))
+      (shell-pop-out)))))
 
 ;;;; ---------------------------------------------------------------------------
 ;;;; multi-term
