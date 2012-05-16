57bc939a553c5e881d67d0f28a551e2b8af96b5c
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-05-16 17:43:28 +0800

    remove unuse file

--- local-lisp/aa.scm
+++
@@ -1,8 +0,0 @@
-(use-modules (system foreign))
-(define libmmr (dynamic-link "./libmmr.so"))
-
-(define ffi-test
-   (pointer->procedure int
-		                            (dynamic-func "ffi_test" libmmr)
-					                         (list int int)))
-(ffi-test 1 2)
--- local-lisp/mmr.c
+++
@@ -1,4 +0,0 @@
-int ffi_test(int a, int b)
-{
- return a+b;
- }
