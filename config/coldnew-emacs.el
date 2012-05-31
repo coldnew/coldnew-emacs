e746c880c7fbf390e0dd3515aeeaa3b527cfc92e
Yen-Chin,Lee <coldnew.tw@gmail.com>
2012-06-01 03:32:58 +0800

    update

--- config/coldnew-emacs.el
+++ config/coldnew-emacs.el
@@ -3,4 +3,10 @@
 
 (setq initial-scratch-message "")
 
+(setq visible-bell t)
+
+(if (featurep 'tool-bar) (tool-bar-mode -1))
+
+(if (featurep 'scroll-bar) (scroll-bar-mode -1))
+
 (provide 'coldnew-emacs)
