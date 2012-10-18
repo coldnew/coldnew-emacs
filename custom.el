10914c0f38a92b97103dce6d96578c0b9a460610
yenchin <yenchin@weintek.com>
2012-10-18 12:26:44 +0800

    update

--- custom.el
+++ custom.el
@@ -4,3 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("6c15b38046708c241ccfc71d7fa2e868b4a958e6cadd8a47eb6776bfeb2d2976" default))))
