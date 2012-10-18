b6f3690106058b7639862c4c1beb2bea34e4e652
Yen-Chin, Lee coldnew <coldnew.tw@gmail.com>
2012-10-18 21:23:14 +0800

    update

--- custom.el
+++ custom.el
@@ -4,3 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-indent-next-pair-timer-interval (quote ((default 0.0005)))))
