42dbe317479f8d4f49229d221b94ba2b70391e2e
Yen-Chin Lee <coldnew.tw@gmail.com>
2014-08-31 20:06:48 +0800

    Clear unuse
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- init.el
+++ init.el
@@ -73,7 +73,7 @@
 ;; Done and done!!
 (message "\nEmacs is ready to serve you, Master %s!\n" (getenv "USER"))
 
-(require 'remote-emacsclient)
-(update-tramp-emacs-server-port-forward tramp-default-method)
+;;(require 'remote-emacsclient)
+;;(update-tramp-emacs-server-port-forward tramp-default-method)
 
 ;;; init.el ends here.
