11f6c2b5ec783968d532c437d1a350a8de0f0e32
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-02-03 00:00:22 +0800

    Make emacs show lambda in greek symbols
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- private/coldnew-editor/config.el
+++ private/coldnew-editor/config.el
@@ -13,28 +13,26 @@
 
 (require 'delsel)
 
-;;; minibuffer
 ;; use bar type cursor instead of box
 (add-hook 'minibuffer-setup-hook '(lambda () (setq cursor-type 'bar)))
 
-
-;;; Scratch
 ;; Create *scratch* automatically
 (run-with-idle-timer 1 t
                      '(lambda ()
                         (unless (get-buffer "*scratch*")
                           (with-current-buffer (get-buffer-create "*scratch*")
                             (lisp-interaction-mode)))))
+;; linum
+(eval-after-load 'linum '(progn (global-linum-mode 1)))
 
-;;; Easy PG
+;; Make emacs show lambda in greek symbols
+(when (and (>= emacs-major-version 24) (>= emacs-minor-version 4))
+  (global-prettify-symbols-mode 1))
+
+;;;; Easy PG
 (eval-after-load 'epa-file
   '(progn
      ;; Control whether or not to pop up the key selection dialog.
      (setq epa-file-select-keys 0)
      ;; Cache passphrase for symmetric encryption.
      (setq epa-file-cache-passphrase-for-symmetric-encryption t)))
-
-;;; linum
-(eval-after-load 'linum
-  '(progn
-     (global-linum-mode 1)))
