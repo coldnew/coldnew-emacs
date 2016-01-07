805dfe7aff8fc275ea416ef777625e4cee01cc01
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-01-07 16:46:02 +0800

    add building script prototype

--- scripts/build-emacs.sh
+++ scripts/build-emacs.sh
@@ -4,15 +4,33 @@
 SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Configration
-INSDIR="${SDIR}/../"
-SRCDIR="${SDIR}/src/emacs"
+INSDIR="${SDIR}/../.build"
+SRCDIR="${SDIR}/../src/emacs"
+
+################################################################################
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+set -e
+
+################################################################################
 
 do_autogen () {
-    ./autogen.h
+    ./autogen.sh
+}
+
+do_configure_cli () {
+    ./configure --prefix=${INSDIR} --with-modules \
+		--without-x \
+		--without-ns \
+
 }
 
 do_configure_osx () {
-    ./configure --prefix=${INSDIR} --without-x --without-ns --with-modules
+    ./configure --prefix=${INSDIR} --with-modules \
+		--with-ns --disable-ns-self-contained \
+		--with-gnutls \
+		--with-rsvg \
+		--with-imagemagick \
+		--without-dbus --without-x
 }
 
 do_configure_linux () {
