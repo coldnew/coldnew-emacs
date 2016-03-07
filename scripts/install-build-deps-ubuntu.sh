8bb6ff3744da269234ef10cbd1e52c77becc76e6
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-03-07 10:44:08 +0800

    remove atk-dev
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- scripts/install-build-deps-ubuntu.sh
+++ scripts/install-build-deps-ubuntu.sh
@@ -5,4 +5,4 @@ sudo apt-get install \
      automake libtool texinfo xorg-dev libgtk2.0-dev \
      libjpeg-dev libncurses-dev libdbus-1-dev libgif-dev \
      libtiff-dev libm17n-dev libpng12-dev librsvg2-dev \
-     libotf-dev libxml2-dev libatk-dev libcairo-dev guile2.0-dev
+     libotf-dev libxml2-dev libcairo-dev
