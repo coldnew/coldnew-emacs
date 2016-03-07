4bfbe1fd80f16a2d3a7e5c1fb863d34a833896a3
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-03-07 10:26:50 +0800

    circle.yml: install emacs deps directly
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ scripts/install-build-deps-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get install \
+     gcc-multilib g++-multilib build-essential autoconf \
+     automake libtool texinfo xorg-dev libgtk2.0-dev \
+     libjpeg-dev libncurses-dev libdbus-1-dev libgif-dev \
+     libtiff-dev libm17n-dev libpng12-dev librsvg2-dev \
+     libotf-dev libxml2-dev libatk-dev libcairo-dev guile2.0-dev
