1c4a2cd3b6ad99c4298de8cd284d247cfa24efa6
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-04-21 10:47:42 +0800

    prototype to integrate again.
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- modules/load-modules.el
+++ modules/load-modules.el
@@ -1,2 +1,28 @@
 ;;; Add all modules to path
 ;; This file will be load in my emacs config.
+
+
+;;;; Spacemacs
+;;; https://github.com/syl20bnr/spacemacs
+
+;; Latest spacemacs can setup `SPACEMACSDIR' to load customize spacemacs init.el file.
+(setenv "SPACEMACSDIR" (concat user-emacs-directory "spacemacs.d"))
+
+;; Make spacemacs not remove my packages.
+(defadvice configuration-layer/delete-orphan-packages (around null-func activate)
+  "Overwrite the spacemacs's `configuration-layer/delete-orphan-packages'
+  to make it not remove any orphan packages.")
+
+;; Spacemacs no need to check newer version
+(defadvice spacemacs/check-for-new-version (around null-func activate)
+  "Overwrite the spacemacs's `spacemacs/check-for-new-version' to
+  makt it useless since I use git submodule to bundle spacemacs with my emacs.")
+
+(require 'f)
+(let* ((spacemacs-dir
+	(directory-file-name (f-join user-emacs-directory "modules" "spacemacs")))
+       (spacemacs-init
+	(concat (file-name-as-directory spacemacs-dir) "init.el"))
+       (user-emacs-directory (file-name-directory spacemacs-init)))
+  ;; Initial spacemacs, our emacs run on top of it
+  (load spacemacs-init))
