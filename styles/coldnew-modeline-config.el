85abf19da7e05efff65e314ac78a43dd2810b511
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-11-25 14:51:22 +0800

    prototype for coldnew-modeline-config
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ styles/coldnew-modeline-config.el
@@ -0,0 +1,32 @@
+;;; coldnew-modeline-config.el --- coldnew's modeline-config.
+
+;; Copyright (C) 2015 - 2016 Yen-Chin, Lee.
+
+;; Author: coldnew <coldnew.tw@gmail.com>
+;; Kyewords: themes
+;; Version: 0.3
+;; X-Original-Version: 0.3
+;; Package-Requires: ((emacs "24.3"))
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(require 'spaceline)
+
+
+
+(provide 'coldnew-modeline-config)
+;;; coldnew-modeline-config.el ends here
