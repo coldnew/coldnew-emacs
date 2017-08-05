0116b491c79aad8a4c736cf4ae1d307bc17997d5
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-08-05 09:27:17 +0800

    coldnew-mode-line-config.el: refactor with spaceline
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- styles/coldnew-modeline-config.el
+++ styles/coldnew-modeline-config.el
@@ -28,114 +28,118 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package telephone-line
+(use-package spaceline :ensure t)
+
+(use-package spaceline-all-the-icons
   :ensure t
   :config
-  ;; Need to create custom segments
-  (require 'telephone-line-utils)
 
-  ;; Set default separators: choose either of them
-  (setq telephone-line-primary-left-separator 'telephone-line-identity-left)
-  (setq telephone-line-primary-right-separator 'telephone-line-identity-right)
-  ;; OR
-  ;; (setq telephone-line-primary-left-separator 'telephone-line-cubed-left)
-  ;; (setq telephone-line-primary-right-separator 'telephone-line-cubed-right)
+  (setq spaceline-all-the-icons-separators-type 'slant)
 
-  ;; Set subseparator
-  (if window-system
-      (progn
-        (setq telephone-line-secondary-left-separator 'telephone-line-identity-hollow-left)
-        (setq telephone-line-secondary-right-separator 'telephone-line-identity-hollow-right)))
+;;; Full Modeline Definition
+  (defconst spaceline-coldnew-theme '("%e" (:eval (spaceline-ml-coldnew)))
+    "Constant version of variable `spaceline-coldnew-theme' to allow to be set manually.")
 
-;;;; Custom segments
+;;;###autoload
+  (defun spaceline-coldnew-theme (&rest additional-segments)
+    "Install the `spaceline-ml-coldnew'.
+Add ADDITIONAL-SEGMENTS to the end of the theme."
+    (interactive)
+    (spaceline-compile
+      "coldnew"
+      '((all-the-icons-anzu
+	 :face mode-line
+	 :skip-alternate t)
 
-  ;; Example of color string segment
-  ;; (telephone-line-defsegment* my-color-segment
-  ;;   (propertize "some-string" 'face `(:foreground "green")))
+	((all-the-icons-modified
+	  all-the-icons-bookmark
+	  all-the-icons-dedicated
+	  all-the-icons-window-number
+	  all-the-icons-buffer-size) :face highlight-face :skip-alternate t)
+
+	all-the-icons-separator-left-active-1
+
+	((all-the-icons-projectile
+	  all-the-icons-mode-icon
+	  ((all-the-icons-buffer-path
+            all-the-icons-buffer-id) :separator ""))
+	 :face default-face)
+
+	all-the-icons-separator-left-active-2
+
+	((all-the-icons-process
+	  all-the-icons-position
+	  all-the-icons-region-info
+	  all-the-icons-fullscreen
+	  all-the-icons-text-scale
+	  all-the-icons-narrowed
+	  all-the-icons-multiple-cursors)
+	 :face highlight-face
+	 :separator (spaceline-all-the-icons--separator spaceline-all-the-icons-primary-separator " "))
+
+	all-the-icons-separator-left-active-3
+	all-the-icons-separator-left-inactive
+
+	((all-the-icons-vc-icon
+	  all-the-icons-vc-status
+	  ((all-the-icons-git-ahead
+            all-the-icons-git-status) :separator " ")
+	  ((all-the-icons-flycheck-status
+            all-the-icons-flycheck-status-info) :separator " ")
+	  all-the-icons-package-updates)
+	 :face other-face
+	 :separator (spaceline-all-the-icons--separator spaceline-all-the-icons-secondary-separator " "))
+
+	all-the-icons-separator-left-active-4
+
+	((all-the-icons-separator-minor-mode-left
+	  all-the-icons-minor-modes
+	  all-the-icons-separator-minor-mode-right)
+	 :tight t
+	 :face highlight-face
+	 :when spaceline-all-the-icons-minor-modes-p)
+
+	((all-the-icons-which-function)
+	 :face powerline-active2
+	 :separator ""))
+
+      `(((,@additional-segments) :when active :face powerline-active2)
+	((,@additional-segments) :when (not active) :face powerline-inactive2)
+
+	((all-the-icons-weather
+	  all-the-icons-temperature
+	  all-the-icons-sunrise
+	  all-the-icons-sunset)
+	 :face powerline-active2
+	 :separator (spaceline-all-the-icons--separator spaceline-all-the-icons-secondary-separator " "))
+
+	((all-the-icons-player-volume
+	  all-the-icons-player-controls
+	  all-the-icons-track
+	  all-the-icons-player-controls-shuffle)
+	 :face powerline-active2)
+
+	all-the-icons-separator-right-active-1
+	((all-the-icons-hud
+	  all-the-icons-buffer-position)
+	 :separator " " :when active)
 
 
-  ;; TODO: Rewrite using assoc and defvar
-  ;; Display major mode
-  (telephone-line-defsegment* my-major-mode-segment
-                              (let ((mode (cond
-                                           ((string= mode-name "Fundamental") "Text")
-                                           ((string= mode-name "Emacs-Lisp") "Elisp")
-                                           ((string= mode-name "Javascript-IDE") "Javascript")
-                                           (t mode-name))))
-                                (propertize mode 'face `(:foreground "#835d83"))))
+	all-the-icons-separator-right-active-2
+	all-the-icons-separator-right-inactive
 
-  ;; Display evil state
-  (telephone-line-defsegment* my-evil-segment
-                              (if (telephone-line-selected-window-active)
-                                  (let ((tag (cond
-                                              ((string= evil-state "normal") ":")
-                                              ((string= evil-state "insert") ">")
-                                              ((string= evil-state "replace") "r")
-                                              ((string= evil-state "visual") "v")
-                                              ((string= evil-state "operator") "=")
-                                              ((string= evil-state "motion") "m")
-                                              ((string= evil-state "emacs") "Emacs")
-                                              ((string= evil-state "multiedit") "Multi")
-                                              (t "-"))))
-                                    tag)))
+	((all-the-icons-battery-status
+	  all-the-icons-time)
+	 :separator (spaceline-all-the-icons--separator spaceline-all-the-icons-primary-separator " ")
+	 :face default-face)))
 
-  ;; Display buffer name
-  (telephone-line-defsegment* my-buffer-segment
-                              `(""
-                                ,(telephone-line-raw mode-line-buffer-identification t)))
+    (setq-default mode-line-format spaceline-coldnew-theme))
 
+  (spaceline-coldnew-theme)
 
-  ;; Display current position in a buffer
-  (telephone-line-defsegment* my-position-segment
-                              (if (telephone-line-selected-window-active)
-                                  (if (eq major-mode 'paradox-menu-mode)
-                                      (telephone-line-trim (format-mode-line mode-line-front-space))
-                                      '(" %3l,%2c "))))
-
-  ;; Ignore some buffers in modeline
-  (defvar modeline-ignored-modes nil
-    "List of major modes to ignore in modeline")
-
-  (setq modeline-ignored-modes '("Dashboard"
-                                 "Warnings"
-                                 "Compilation"
-                                 "EShell"
-                                 "REPL"
-                                 "Messages"))
-
-  ;; Display modified status
-  (telephone-line-defsegment* my-modified-status-segment
-                              (if (and (buffer-modified-p) (not (member mode-name modeline-ignored-modes)))
-                                  (propertize "+" 'face `(:foreground "#85b654"))
-                                  ""))
-
-  ;; Display encoding system
-  (telephone-line-defsegment* my-coding-segment
-                              (if (telephone-line-selected-window-active)
-                                  (let* ((code (symbol-name buffer-file-coding-system))
-                                         (eol-type (coding-system-eol-type buffer-file-coding-system))
-                                         (eol (cond
-                                               ((eq 0 eol-type) "unix")
-                                               ((eq 1 eol-type) "dos")
-                                               ((eq 2 eol-type) "mac")
-                                               (t ""))))
-                                    (concat eol " "))))
-
-  ;; Left edge
-  (setq telephone-line-lhs
-        '((accent . "  ")
-          (evil   . (my-evil-segment))
-          (nil    . (my-buffer-segment))
-          (nil    . (my-modified-status-segment))))
-
-  ;; Right edge
-  (setq telephone-line-rhs
-        '((nil     . (telephone-line-misc-info-segment))
-          (accent  . (my-position-segment))
-          (nil     . (my-major-mode-segment))
-          (accent  . (my-coding-segment))))
-
-  (telephone-line-mode 1))
+  ;; force update mode-line
+  (force-mode-line-update)
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
