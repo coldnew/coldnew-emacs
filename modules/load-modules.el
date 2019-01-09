51d85a841784a1ee1b84f196e2f3268fe5397ab4
Yen-Chin Lee <coldnew.tw@gmail.com>
2019-01-09 14:20:12 +0800

    modules: add emacs-libvterm support
    To use this feature, we need to enable --dynamic-module support in emacs.
    
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- modules/load-modules.el
+++ modules/load-modules.el
@@ -1,2 +1,8 @@
 ;;; Add all modules to path
 ;; This file will be load in my emacs config.
+
+;; libvterm
+(add-to-list 'load-path
+	     (concat user-modules-directory "libvterm"))
+(let (vterm-install)
+  (require 'vterm))
