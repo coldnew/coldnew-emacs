236e974b5a8242e30ef2d80ac21735fcc8bb6573
Yen-Chin Lee <coldnew.tw@gmail.com>
2020-01-07 11:31:03 +0800

    build-emacs.sh: add --with-nativecomp support
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- scripts/build-emacs.sh
+++ scripts/build-emacs.sh
@@ -38,7 +38,9 @@ do_configure_linux () {
                 --with-gnutls \
                 --with-rsvg \
                 --with-imagemagick \
-                --with-dbus --with-x --with-x-toolkit=gtk3 --with-xwidgets
+		--with-cairo \
+                --with-dbus --with-x --with-x-toolkit=gtk3 --with-xwidgets \
+		--with-nativecomp
 }
 
 do_make () {
