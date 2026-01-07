1b68b7e1a70689306f7a520c931062512c57de97
Yen-Chin, Lee <coldnew.tw@gmail.com>
2026-01-07 15:58:55 +0800

    refactor: Remove libvterm module

--- modules/load-modules.el
+++
@@ -1,8 +0,0 @@
-;;; Add all modules to path
-;; This file will be load in my emacs config.
-
-;; libvterm
-(add-to-list 'load-path
-	     (concat user-modules-directory "libvterm"))
-(let (vterm-install)
-  (require 'vterm))
