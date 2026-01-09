6249d1c899b622511b34ec94d58509793f82d416
Yen-Chin, Lee <coldnew.tw@gmail.com>
2026-01-09 17:00:26 +0800

    refactor: Remove the lorem ipsum insertion function

--- init.el
+++ init.el
@@ -1076,19 +1076,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (open-line 1)
   (forward-line 1))
 
-;; *** Insert lorem ipsum
-
-(defun my/insert-lorem ()
-  "Insert a lorem ipsum."
-  (interactive)
-  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-          "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
-          "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-          "aliquip ex ea commodo consequat. Duis aute irure dolor in "
-          "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-          "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-          "culpa qui officia deserunt mollit anim id est laborum."))
-
 ;; *** Delete word
 
 (defun my/delete-word (arg)
