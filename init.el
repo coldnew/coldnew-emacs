4ea153d9ac1e97058935a1d0b1b5e34f18f23eab
Yen-Chin, Lee <coldnew.tw@gmail.com>
2026-01-09 17:39:59 +0800

    style: Correct capitalization in Emacs docstrings

--- init.el
+++ init.el
@@ -53,13 +53,13 @@
 
 (defconst user-emacs-directory
   (file-name-directory (or load-file-name (buffer-file-name)))
-  "My emacs config directory.")
+  "My Emacs config directory.")
 
 ;;   Setup the cache directory to store some cache content.
 
 (defconst user-cache-directory
   (file-name-as-directory (concat user-emacs-directory ".cache"))
-  "My emacs storage area for persistent files.")
+  "My Emacs storage area for persistent files.")
 ;; create the `user-cache-directory' if not exists
 (make-directory user-cache-directory t)
 
